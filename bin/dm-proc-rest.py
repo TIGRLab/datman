45de40eae67b73ddec952b478112cd4b46c8a832
Joseph Viviano <joseph@viviano.ca>
2015-07-21 23:26:23 -0400

    overhaul of resting state module -- now produces multiple outputs

--- bin/dm-proc-rest.py
+++ bin/dm-proc-rest.py
@@ -1,14 +1,41 @@
 #!/usr/bin/env python
 """
-dm-proc-rest.py <experiment-directory> <scratch-directory> <script>
+This pre-processes resting state data and extracts the mean time series from the defined
+ROIs in MNI space (6 mm spheres). This data is returned as the time series, a full correlation
+matrix, and a partial correlation matrix (all in .csv format). 
 
-Runs the defined epitome script on the resting-state data.
+Usage:
+    dm-proc-rest.py [options] <project> <tmppath> <script> <assets>
 
-Requires AFNI, FSL, modules to be loaded.
-Requires freesurfer.py to have been completed.
+Arguments: 
+    <project>           Full path to the project directory containing data/.
+    <tmppath>           Full path to a shared folder to run 
+    <script>            Full path to an epitome-style script.
+    <assets>            Full path to an assets folder containing rsfc.labels (3dUndump format).
 
-outputs are placed in <experiment-directory>/data/rest.
-logs in data_path/logs/rest.
+Options:
+    -v,--verbose             Verbose logging
+    --debug                  Debug logging
+
+DETAILS
+
+    1) Preprocesses fMRI data using the defined epitome-style script.
+    2) Produces a CSV of the ROI time series from the 'rsfc.labels' file in 'assets/' (6mm sphere).
+    3) Produces a correlation and partial correlation matrix of these same time series.
+
+    Each subject is run through this pipeline if the outputs do not already exist.
+    Outputs are placed in <project>/data/rest
+    Logs in <project>/logs/rest
+
+DEPENDENCIES
+
+    + python
+    + afni
+    + fsl
+
+    Requires dm-proc-freesurfer.py to be completed.
+
+This message is printed with the -h, --help flags.
 """
 
 import os, sys
@@ -17,11 +44,73 @@ from random import choice
 from glob import glob
 from string import ascii_uppercase, digits
 import numpy as np
+from scipy import stats, linalg
 import nibabel as nib
 import StringIO as io
 import matplotlib.pyplot as plt
 import datman as dm
 import tempfile as tmp
+from datman.docopt import docopt
+
+def partial_corr(C):
+    """
+    Partial Correlation in Python (clone of Matlab's partialcorr)
+    from https://gist.github.com/fabianp/9396204419c7b638d38f
+
+    This uses the linear regression approach to compute the partial 
+    correlation (might be slow for a huge number of variables). The 
+    algorithm is detailed here:
+
+        http://en.wikipedia.org/wiki/Partial_correlation#Using_linear_regression
+
+    Taking X and Y two variables of interest and Z the matrix with all the variable minus {X, Y},
+    the algorithm can be summarized as
+
+        1) perform a normal linear least-squares regression with X as the target and Z as the predictor
+        2) calculate the residuals in Step #1
+        3) perform a normal linear least-squares regression with Y as the target and Z as the predictor
+        4) calculate the residuals in Step #3
+        5) calculate the correlation coefficient between the residuals from Steps #2 and #4; 
+
+    The result is the partial correlation between X and Y while controlling for the effect of Z
+
+    Returns the sample linear partial correlation coefficients between pairs of variables in C, controlling 
+    for the remaining variables in C.
+
+
+    Parameters
+    ----------
+    C : array-like, shape (n, p)
+        Array with the different variables. Each column of C is taken as a variable
+
+
+    Returns
+    -------
+    P : array-like, shape (p, p)
+        P[i, j] contains the partial correlation of C[:, i] and C[:, j] controlling
+        for the remaining variables in C.
+    """
+    
+    C = np.asarray(C)
+    p = C.shape[1]
+    P_corr = np.zeros((p, p), dtype=np.float)
+    for i in range(p):
+        P_corr[i, i] = 1
+        for j in range(i+1, p):
+            idx = np.ones(p, dtype=np.bool)
+            idx[i] = False
+            idx[j] = False
+            beta_i = linalg.lstsq(C[:, idx], C[:, j])[0]
+            beta_j = linalg.lstsq(C[:, idx], C[:, i])[0]
+
+            res_j = C[:, j] - C[:, idx].dot( beta_i)
+            res_i = C[:, i] - C[:, idx].dot(beta_j)
+            
+            corr = stats.pearsonr(res_i, res_j)[0]
+            P_corr[i, j] = corr
+            P_corr[j, i] = corr
+        
+    return P_corr
 
 def proc_data(sub, data_path, tmp_path, tmpdict, script):
     """
@@ -81,7 +170,7 @@ def proc_data(sub, data_path, tmp_path, tmpdict, script):
         rest_data = filter(lambda x: 'rst' in x.lower(), niftis)
         
         if len(rest_data) > 1:
-            print('*** Multiple resting-state data! Using most recent ' + sub) 
+            print('MSG: Multiple resting-state data! Using most recent for {}'.format(sub)) 
 
         rest_data = rest_data[-1]
 
@@ -89,15 +178,9 @@ def proc_data(sub, data_path, tmp_path, tmpdict, script):
         print('ERROR: No REST data found for ' + str(sub))
         raise ValueError
 
-    # check if output already exists, abort if so
-    if os.path.isfile(os.path.join(rest_path, sub + '_complete.log')) == True:
-        return None
-
     # copy data into temporary epitome structure
     tmpfolder = tmp.mkdtemp(prefix='rest-', dir=tmp_path)
     tmpdict[sub] = tmpfolder
-
-    print(tmpfolder)
 
     dm.utils.make_epitome_folders(tmpfolder, 1)
     dm.utils.run('cp {t1_path}/{t1_data} {tmpfolder}/TEMP/SUBJ/T1/SESS01/anat_T1_brain.nii.gz'.format(t1_path=t1_path, t1_data=t1_data, tmpfolder=tmpfolder))
@@ -107,7 +190,7 @@ def proc_data(sub, data_path, tmp_path, tmpdict, script):
 
     # submit to queue
     uid = ''.join(choice(ascii_uppercase + digits) for _ in range(6))
-    cmd = 'bash {} {}'.format(script, tmpfolder)
+    cmd = 'bash {} {} 4 '.format(script, tmpfolder)
     name = 'datman_rest_{}_{}'.format(sub, uid)
     log = os.path.join(data_path, 'logs/rest')
     cmd = 'echo {cmd} | qsub -o {log} -S /bin/bash -V -q main.q -cwd -N {name} -l mem_free=3G,virtual_free=3G -j y'.format(cmd=cmd, log=log, name=name)
@@ -115,45 +198,81 @@ def proc_data(sub, data_path, tmp_path, tmpdict, script):
 
     return name, tmpdict
 
-def export_data(sub, folder, rest_path):
+def check_returncode(returncode):
+    if returncode != 0:
+        raise ValueError
 
-    dm.utils.run('cp {folder}/TEMP/SUBJ/FUNC/SESS01/func_MNI-nonlin.DATMAN.01.nii.gz {rest_path}/{sub}_func_MNI-nonlin.01.nii.gz'.format(folder=folder, rest_path=rest_path, sub=sub))
-    dm.utils.run('cp {folder}/TEMP/SUBJ/FUNC/SESS01/anat_EPI_mask_MNI-nonlin.nii.gz {rest_path}/{sub}_anat_EPI_mask_MNI.nii.gz'.format(folder=folder, rest_path=rest_path, sub=sub))
-    dm.utils.run('cp {folder}/TEMP/SUBJ/FUNC/SESS01/reg_T1_to_TAL.nii.gz {rest_path}/{sub}_reg_T1_to_MNI-lin.nii.gz'.format(folder=folder, rest_path=rest_path, sub=sub))
-    dm.utils.run('cp {folder}/TEMP/SUBJ/FUNC/SESS01/reg_nlin_TAL.nii.gz {rest_path}/{sub}_reg_nlin_MNI.nii.gz'.format(folder=folder, rest_path=rest_path, sub=sub))
-    dm.utils.run('cat {folder}/TEMP/SUBJ/FUNC/SESS01/PARAMS/motion.DATMAN.01.1D > {rest_path}/{sub}_motion.1D'.format(folder=folder, rest_path=rest_path, sub=sub))
+def export_data(sub, tmpfolder, rest_path):
+    
+    try:
+        tmppath = os.path.join(tmpfolder, 'TEMP', 'SUBJ', 'FUNC', 'SESS01')
+        print('cp {tmppath}/func_MNI-nonlin.DATMAN.01.nii.gz {rest_path}/{sub}_func_MNI-nonlin.01.nii.gz'.format(tmppath=tmppath, rest_path=rest_path, sub=sub))
 
+        returncode, _, _ = dm.utils.run('cp {tmppath}/func_MNI-nonlin.DATMAN.01.nii.gz {rest_path}/{sub}_func_MNI-nonlin.01.nii.gz'.format(tmppath=tmppath, rest_path=rest_path, sub=sub))
+        check_returncode(returncode)
+        returncode, _, _ = dm.utils.run('cp {tmppath}/anat_EPI_mask_MNI-nonlin.nii.gz {rest_path}/{sub}_anat_EPI_mask_MNI.nii.gz'.format(tmppath=tmppath, rest_path=rest_path, sub=sub))
+        check_returncode(returncode)
+        returncode, _, _ = dm.utils.run('cp {tmppath}/reg_T1_to_TAL.nii.gz {rest_path}/{sub}_reg_T1_to_MNI-lin.nii.gz'.format(tmppath=tmppath, rest_path=rest_path, sub=sub))
+        check_returncode(returncode)
+        returncode, _, _ = dm.utils.run('cp {tmppath}/reg_nlin_TAL.nii.gz {rest_path}/{sub}_reg_nlin_MNI.nii.gz'.format(tmppath=tmppath, rest_path=rest_path, sub=sub))
+        check_returncode(returncode)
+        returncode, _, _ = dm.utils.run('cat {tmppath}/PARAMS/motion.DATMAN.01.1D > {rest_path}/{sub}_motion.1D'.format(tmppath=tmppath, rest_path=rest_path, sub=sub))
+        returncode, _, _ = dm.utils.run('touch {rest_path}/{sub}_preproc-complete.log'.format(rest_path=rest_path, sub=sub))
+        check_returncode(returncode)
+        returncode, _, _ = dm.utils.run('rm -r ' + tmpfolder)
+        check_returncode(returncode)
+
+    except:
+        raise ValueError
     # TODO
     #
     # # copy out QC images of registration
-    # dm.utils.run('cp {folder}/TEMP/SUBJ/FUNC/SESS01/'
+    # dm.utils.run('cp {tmpfolder}/TEMP/SUBJ/FUNC/SESS01/'
     #                         + 'qc_reg_EPI_to_T1.pdf ' +
     #               data_path + '/rest/' + sub + '_qc_reg_EPI_to_T1.pdf')
-    # dm.utils.run('cp {folder}/TEMP/SUBJ/FUNC/SESS01/'
+    # dm.utils.run('cp {tmpfolder}/TEMP/SUBJ/FUNC/SESS01/'
     #                         + 'qc_reg_T1_to_MNI.pdf ' +
     #               data_path + '/rest/' + sub + '_qc_reg_T1_to_MNI.pdf')
 
-    dm.utils.run('touch {rest_path}/{sub}_complete.log'.format(rest_path=rest_path, sub=sub))
-    dm.utils.run('rm -r ' + folder)
 
-def generate_analysis_script(sub, data_path, code_path):
+def analyze_data(sub, assets, rest_path):
     """
-    This will eventually do some graph theory stuff. For now, it will lol.
-
+    Extracts: time series, correlation / partial correlation matricies using labels defined
+    in 'rsfc.labels' in assets/. This file should be formatted for 3dUndump.
     """
-    # first, determine input functional files
-    # niftis = filter(lambda x: 'nii.gz' in x and sub + '_func' in x, 
-    #                 os.listdir(os.path.join(data_path, 'imob')))
-    # niftis.sort()
+    labelfile = os.path.join(assets, 'rsfc.labels')
+    if os.path.isfile(labelfile) == False:
+        raise ValueError
 
-    # input_data = ''
+    dm.utils.run('3dUndump -master {rest_path}/{sub}_anat_EPI_mask_MNI.nii.gz -xyz -srad 6 -prefix {rest_path}/{sub}_rois.nii.gz {labels}'.format(rest_path=rest_path, sub=sub, labels=os.path.join(assets, 'rsfc.labels')))
+    rois, _, _, _ = dm.utils.loadnii('{rest_path}/{sub}_rois.nii.gz'.format(rest_path=rest_path, sub=sub))
+    data, _, _, _ = dm.utils.loadnii('{rest_path}/{sub}_func_MNI-nonlin.01.nii.gz'.format(rest_path=rest_path, sub=sub))
 
-    # for nifti in niftis:
-    #     input_data = input_data + data_path + '/imob/' + nifti + ' '
+    n_rois = len(np.unique(rois[rois > 0]))
+    dims = np.shape(data)
 
-    print('lol')
+    output = np.zeros((n_rois, dims[1]))
 
-def main(base_path, tmp_path, script):
+    for i, roi in enumerate(np.unique(rois[rois > 0])):
+        idx = np.where(rois == roi)[0]
+        
+        if len(idx) > 0:
+            output[i, :] = np.mean(data[idx, :], axis=0)
+
+    # save the raw time series
+    np.savetxt('{rest_path}/{sub}_roi-timeseries.csv'.format(rest_path=rest_path, sub=sub), output.transpose(), delimiter=',')
+
+    # save the full correlation matrix
+    corrs = np.corrcoef(output)
+    np.savetxt('{rest_path}/{sub}_roi-corrs.csv'.format(rest_path=rest_path, sub=sub), corrs, delimiter=',')
+    
+    # save partial correlation matrix
+    pcorrs = partial_corr(output.transpose())
+    np.savetxt('{rest_path}/{sub}_roi-pcorrs.csv'.format(rest_path=rest_path, sub=sub), pcorrs, delimiter=',') 
+
+    dm.utils.run('touch {rest_path}/{sub}_analysis-complete.log'.format(rest_path=rest_path, sub=sub))
+
+def main():
     """
     Essentially, analyzes the resting-state data.
 
@@ -163,14 +282,23 @@ def main(base_path, tmp_path, script):
     4) Generates an experiment-wide correlation matrix.
     5) Generates a set of graph metrics for each subject. 
     """
+
+    global VERBOSE 
+    global DEBUG
+    arguments  = docopt(__doc__)
+    project    = arguments['<project>']
+    tmp_path   = arguments['<tmppath>']
+    script     = arguments['<script>']
+    assets     = arguments['<assets>']
+
     # sets up paths
-    data_path = dm.utils.define_folder(os.path.join(base_path, 'data'))
+    data_path = dm.utils.define_folder(os.path.join(project, 'data'))
     nii_path = dm.utils.define_folder(os.path.join(data_path, 'nii'))
     t1_path = dm.utils.define_folder(os.path.join(data_path, 't1'))
     rest_path = dm.utils.define_folder(os.path.join(data_path, 'rest'))
     tmp_path = dm.utils.define_folder(tmp_path)
-    _ = dm.utils.define_folder(os.path.join(base_path, 'logs'))
-    log_path = dm.utils.define_folder(os.path.join(base_path, 'logs/rest'))
+    _ = dm.utils.define_folder(os.path.join(project, 'logs'))
+    log_path = dm.utils.define_folder(os.path.join(project, 'logs/rest'))
 
     list_of_names = []
     tmpdict = {}
@@ -179,9 +307,9 @@ def main(base_path, tmp_path, script):
     # loop through subjects
     for sub in subjects:
 
-        if dm.scanid.is_phantom(sub) == True: continue
-        if os.path.isfile(os.path.join(
-                          rest_path,  sub + '_complete.log')) == True:
+        if dm.scanid.is_phantom(sub) == True: 
+            continue
+        if os.path.isfile(os.path.join(rest_path,  '{}_preproc-complete.log'.format(sub))) == True:
             continue
 
         try:
@@ -190,7 +318,7 @@ def main(base_path, tmp_path, script):
             list_of_names.append(name)
 
         except ValueError as ve:
-            print('ERROR: ' + str(sub) + ' !!!')
+            print('ERROR: Failed to preprocess {}'.format(sub))
 
     if list_of_names == []:
         sys.exit()
@@ -200,16 +328,20 @@ def main(base_path, tmp_path, script):
 
     # copy functionals, registrations, motion parameters to rest folder.
     for sub in subjects:
-        if dm.scanid.is_phantom(sub) == True: continue
-        if os.path.isfile(os.path.join(
-                          rest_path,  sub + '_complete.log')) == False:
+        if dm.scanid.is_phantom(sub) == True: 
+            continue
+        if os.path.isfile(os.path.join(rest_path,  sub + '_preproc-complete.log')) == False:
             try:
-                export_data(sub, os.path.join(tmp_path, tmpdict[sub]), rest_path)
-            except:
+                export_data(sub, tmpdict[sub], rest_path)
+            except ValueError as ve:
+                print('ERROR: Failed to export {}'.format(sub))
                 continue
 
+        if os.path.isfile(os.path.join(rest_path,  sub + '_analysis-complete.log')) == False:
+            try:
+                analyze_data(sub, assets, rest_path)
+            except ValueError as ve:
+                print('ERROR: Failed to extract time-series and connectivity data from pre-processed data.')
+
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        main(sys.argv[1], sys.argv[2], sys.argv[3])
-    else:
-        print(__doc__)
+    main()
