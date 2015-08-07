5f723444248f407c11212b749efc49ecc96ff886
Erin W Dickie <erin.w.dickie@gmail.com>
2015-08-07 12:52:48 -0400

    almost there - but buggy

--- bin/dm-proc-enigmaconcat.py
+++ bin/dm-proc-enigmaconcat.py
@@ -10,6 +10,8 @@ Arguments:
     <outputdir>        Top directory for the output file structure
 
 Options:
+  --gen-results            Genereate a new resutls file from the available data
+  --ROItxt-tag STR         String within the individual participants results that identifies their data (default = 'ROIout_avg')
   --results FILE           Filename for the results csv output
   -v,--verbose             Verbose logging
   --debug                  Debug logging in Erin's very verbose style
@@ -36,6 +38,8 @@ import datetime
 arguments       = docopt(__doc__)
 outputdir       = arguments['<outputdir>']
 resultsfile     = arguments['--results']
+GENresults      = arguments['--gen-results']
+ROItxt_tag      = arguments['--ROItxt-tag']
 VERBOSE         = arguments['--verbose']
 DEBUG           = arguments['--debug']
 DRYRUN          = arguments['--dry-run']
@@ -45,22 +49,59 @@ if DEBUG: print arguments
 ## if no result file is given use the default name
 outputdir = os.path.normpath(outputdir)
 if resultsfile == None:
-    resultsfile = os.path.join(outputdir,ENIGMA-DTI-results.csv)
+    resultsfile = os.path.join(outputdir,'ENIGMA-DTI-results.csv')
+if ROItxt_tag == None: ROItxt_tag = '_ROIout_avg'
+
+SUBFOLDERS = True ## assume that the file is inside a heirarchy that contains folders with subject names
+## find the files that match the resutls tag...first using the place it should be from doInd-enigma-dti.py
+ROIfiles = glob.glob(outputdir + '/*/*/*' +  ROItxt_tag + '*')
+## if that doesn't work, try the pattern from ENIGMA_MASTER.sh (it could be old data)
+if len(ROIfiles) == 0:
+    ROIfiles = glob.glob(outputdir + '/*/*' +  ROItxt_tag + '*')
+    SUBFOLDERS = False #there are probaly not individual subject folders
+## if that doesn't work - try one more level up...
+if len(ROIfiles) == 0:
+    ROIfiles = glob.glob(outputdir + '/*' +  ROItxt_tag + '*')
+## if we still haven't found any files..give up and exit
+if len(ROIfiles) == 0:
+    sys.exit('Could not find any csv files with tag *{}*'.format(ROItxt_tag))
+if DEBUG: print ROIfiles
+
+## load the first ROIfile to get column header info
+firstROItxt = pd.read_csv(ROIfiles[0], sep=',', dtype=str, comment='#')
+tractnames = firstROItxt['Tract'].tolist() # reads the tract names from the 'Tract' column for template
 
 ####set checklist dataframe structure here
 #because even if we do not create it - it will be needed for newsubs_df (line 80)
-cols = ["id", "FA_nii", "date_ran", "run",\
-    "ACR-L","ACR-R","ALIC-L","ALIC-R","AverageFA","BCC","CGC","CGC-L","CGC-R",\
-    "CR","CR-L","CR-R","CST","CST-L","CST-R","EC","EC-L","EC-R","FX","GCC",\
-    "IC","IC-L","IC-R","IFO","IFO-L","IFO-R","PCR-L","PCR-R","PLIC-L","PLIC-R",\
-    "PTR","PTR-L","PTR-R","RLIC-L","RLIC-R","SCC","SCR-L","SCR-R","SFO","SFO-L",\
-    "SFO-R","SLF","SLF-L","SLF-R","SS","SS-L","SS-R","UNC-L","UNC-R", \
-    "qc_rator", "qc_rating", "notes"]
-
 # if the checklist exists - open it, if not - create the dataframe
-if os.path.isfile(resultsfile):
-	results = pd.read_csv(results, sep=',', dtype=str, comment='#')
+if GENresults == False:
+    if os.path.isfile(resultsfile):
+        ## read in the file
+    	results = pd.read_csv(resultsfile, sep=',', dtype=str, comment='#')
+        cols = list(results.columns.values)
+        if len(set(tractnames) & set(cols)) < len(tractnames):
+            print("warning - not all tractnames in header...")
+    else:
+        sys.exit('Could not find {}'.format(resultsfile))
 else:
-	results = pd.DataFrame(columns = cols)
+    cols = ['id'] + tractnames
+    results = pd.DataFrame(columns = cols)
 
 ###now need to insert the new dataness...
+for csvfile in ROIfiles:
+    csvdata = pd.read_csv(csvfile, sep=',', dtype=str, comment='#')
+    if SUBFOLDERS == True:
+        this_id = os.path.basename(os.path.dirname(os.path.dirname(csvfile)))
+    else:
+        this_id = os.path.basename(csvfile)
+    if this_id in results.id:
+        idx = results[results.id == this_id].index[0]
+    else:
+        idx = len(results)
+        results = results.append(pd.DataFrame(columns = cols, index = [idx]))
+        results.id[idx] = this_id
+    for tractname in tractnames:
+        results[tractname][idx] = csvdata['Average'][csvdata['Tract']==tractname]
+
+## write the checklist out to a file
+results.to_csv(resultsfile, sep=',', columns = cols, index = False)
