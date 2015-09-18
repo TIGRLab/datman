4b79595963471216858430a4badac6a2afb48b3e
Joseph Viviano <joseph@viviano.ca>
2015-09-18 14:24:26 -0400

    added figure closing to prevent max_figure warnings in matplotlib

--- bin/dm-proc-ea.py
+++ bin/dm-proc-ea.py
@@ -355,7 +355,8 @@ def process_behav_data(log, assets, func_path, sub, trial_type):
     fig.suptitle(log, size=10)
     fig.set_tight_layout(True)
     fig.savefig('{func_path}/{sub}/{sub}_{logname}.pdf'.format(func_path=func_path, sub=sub, logname=os.path.basename(log)[:-4]))
-    #fig.close()
+    plt.clf(fig)
+    plt.close(fig)
 
     return onsets_used, durations, correlations, button_pushes
 
