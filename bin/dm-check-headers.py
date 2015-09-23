75969b5e074cd87675d5b6123c2020cfbe99e506
Erin Dickie <edickie@tesla.clarke-inst.on.ca>
2015-09-23 12:42:07 -0400

    changing up the logging - need to test

--- bin/dm-check-headers.py
+++ bin/dm-check-headers.py
@@ -3,26 +3,28 @@
 Diffs the relevant fields of the header to determine if anything has changed in
 the protocol.
 
-Usage: 
+Usage:
     dm-check-header.py [options] <standards> <exam>...
 
-Arguments: 
-    <standards/>            Folder with subfolders named by tag. Each subfolder
-                            has a sample gold standard dicom file for that tag.
+Arguments:
+    <standards/>             Folder with subfolders named by tag. Each subfolder
+                             has a sample gold standard dicom file for that tag.
 
-    <exam/>                 Folder with one dicom file sample from each series
-                            to check
+    <exam/>                  Folder with one dicom file sample from each series
+                             to check
 
-Options: 
-    --quiet                 Don't print warnings
-    --ignore-headers LIST   Comma delimited list of headers to ignore
+Options:
+    --quiet                  Don't print warnings
+    --verbose                Print warnings
+    --blacklist-fails FILE   Append series that fail to the blacklist (requires path to blacklist.csv)
+    --ignore-headers LIST    Comma delimited list of header fields to ignore
 
 DETAILS
-    
-    Output looks like: 
 
-    /path/to/exam/dicom.dcm: headers not in standard: list,... 
-    /path/to/exam/dicom.dcm: headers not in series: list,... 
+    Output looks like:
+
+    /path/to/exam/dicom.dcm: headers not in standard: list,...
+    /path/to/exam/dicom.dcm: headers not in series: list,...
     /path/to/exam/dicom.dcm: header {}: expected = {}, actual = {} [tolerance = {}]
 
 
@@ -34,6 +36,7 @@ import datman as dm
 import numpy as np
 import datman.utils
 import os.path
+import pandas as pd
 
 ignored_headers = set([
     'AcquisitionDate',
@@ -93,7 +96,7 @@ ignored_headers = set([
 
 integer_tolerances = {
         # field           : interger difference
-        'ImagingFrequency': 1, 
+        'ImagingFrequency': 1,
         'EchoTime': 5,
 }
 
@@ -102,12 +105,12 @@ decimal_tolerances = {
 }
 
 
-QUIET = False
+# QUIET = False
 
 def compare_headers(stdpath, stdhdr, cmppath, cmphdr, ignore=ignored_headers):
     """
-    Accepts two pydicom objects and prints out header value differences. 
-    
+    Accepts two pydicom objects and prints out header value differences.
+
     Headers in ignore set are ignored.
     """
 
@@ -118,15 +121,20 @@ def compare_headers(stdpath, stdhdr, cmppath, cmphdr, ignore=ignored_headers):
     only_stdhdr = stdhdr_.difference(cmphdr_)
     only_cmphdr = cmphdr_.difference(stdhdr_)
     both_hdr    = stdhdr_.intersection(cmphdr_)
-  
+
+    # create string object to hold errors
+    errors = []
+
     if only_stdhdr:
-        print("{cmppath}: headers in series, not in standard: {list}".format(
-            cmppath=cmppath, list=", ".join(only_stdhdr)))
+        thiserror = "{cmppath}: headers in series, not in standard: {list}".format(
+            cmppath=cmppath, list=", ".join(only_stdhdr))
+        errors.append(thiserror)
 
     if only_cmphdr:
-        print("{cmppath}: headers in standard, not in series: {list}".format(
-            cmppath=cmppath, list=", ".join(only_cmphdr)))
-    
+        thiserror = "{cmppath}: headers in standard, not in series: {list}".format(
+            cmppath=cmppath, list=", ".join(only_cmphdr))
+        errors.append(thiserror)
+
     for header in both_hdr:
         stdval = stdhdr.get(header)
         cmpval = cmphdr.get(header)
@@ -141,7 +149,7 @@ def compare_headers(stdpath, stdhdr, cmppath, cmphdr, ignore=ignored_headers):
 
             if difference > n:
                 msg = "{}: header {}, expected = {}, actual = {} [tolerance = {}]"
-                print(msg.format(cmppath, header, stdval_rounded, cmpval_rounded, n))
+                errors.append(msg.format(cmppath, header, stdval_rounded, cmpval_rounded, n))
 
         elif header in decimal_tolerances:
             n = decimal_tolerances[header]
@@ -151,61 +159,107 @@ def compare_headers(stdpath, stdhdr, cmppath, cmphdr, ignore=ignored_headers):
 
             if stdval_rounded != cmpval_rounded:
                 msg = "{}: header {}, expected = {}, actual = {} [tolerance = {}]"
-                print(msg.format(cmppath, header, stdval_rounded, cmpval_rounded, n))
+                errors.append(msg.format(cmppath, header, stdval_rounded, cmpval_rounded, n))
 
         elif str(cmpval) != str(stdval):
-            print("{}: header {}, expected = {}, actual = {}".format(
+            errors.append("{}: header {}, expected = {}, actual = {}".format(
                     cmppath, header, stdval, cmpval))
 
+    # if any errors occured - then write them to a log file
+    if len(errors) > 0:
+        with open(cmppath + '.ckheadersfailed', "w") as errorlog:
+            for item in errors:
+                errorlog.write("%s\n" % item)
+        if QUIET == False:
+            print('/n'.join(errors))
 
-def compare_exam_headers(std_headers, examdir, ignorelist):
+
+
+
+
+def compare_exam_headers(std_headers, examdir, ignorelist, blacklist):
     """
     Compares headers for each series in an exam against gold standards
 
     <std_headers> is a map from description -> (path, headers) of all of the
-    standard headers to compare against. 
+    standard headers to compare against.
 
-    <ignorelist> is a list of headers, in addition to the defaults, to ignore. 
+    <ignorelist> is a list of headers, in addition to the defaults, to ignore.
+
+    <blacklist> pandas dataframe to update with header to fail
     """
     exam_headers = dm.utils.get_all_headers_in_folder(examdir)
 
     ignore = ignored_headers.union(ignorelist)
 
+    ## make list to capure new errors
+    newerrors = []
     for path, header in exam_headers.iteritems():
         ident, tag, series, description = dm.scanid.parse_filename(path)
 
         if tag not in std_headers:
-            if not QUIET: 
+            if not QUIET:
                 print("WARNING: {}: No matching standard for tag '{}'".format(
                 path, tag))
             continue
 
         std_path, std_header = std_headers[tag]
 
-        compare_headers(std_path, std_header, path, header, ignore)
+        ### compare the headers for each header in exam series
+        if not os.file.exists(cmppath + '.ckheadersfailed'):
+            compare_headers(std_path, std_header, path, header, ignore)
+            ## if new error logs were created make a note to blurp out
+            if os.file.exists(cmppath + '.ckheadersfailed'):
+                newerrors.append(tag)
+                ## add the info to the blacklist
+                stem = os.path.basename(cmppath).replace('.dcm','')
+                newdata = pd.DataFrame([[stem,'header-not-matching']],columns=['series', 'reason'])
+                blacklist.append(newdata,ignore_index=True)
+
+    #write message to log to report that an error occured
+    if len(newerrors) > 1 :
+        print("{} failed check headers of tags: {}".format(examdir,','.join(newerrors)))
 
 def main():
     global QUIET
+    global VERBOSE
     arguments = docopt(__doc__)
 
-    QUIET = arguments['--quiet']
+    QUIET         = arguments['--quiet']
+    VERBOSE       = arguments['--verbose']
+    standardsdir  = arguments['<standards>']
+    examdirs      = arguments['<exam>']
+    blacklistfile = arguments['--blacklist-fails']
+    ignorelist    = arguments['--ignore-headers']
 
-    standardsdir = arguments['<standards>']
-    examdirs     = arguments['<exam>']
-    ignorelist   = arguments['--ignore-headers']
-    
     if ignorelist:
         ignorelist = ignorelist.split(",")
     else:
         ignorelist = []
 
     manifest = dm.utils.get_all_headers_in_folder(standardsdir,recurse=True)
-   
-    # map tag name to headers 
+
+    # map tag name to headers
     stdmap = { os.path.basename(os.path.dirname(k)):(k,v) for (k,v) in manifest.items()}
 
+    ## if blacklistfile was given load it - if not make a new one
+    if blacklistfile != None:
+        if os.path.isfile(checklistfile):
+            bl = pd.read_table(blacklistfile, sep='\s*', engine="python")
+        else:
+            print('WARNING: could not find blacklistfile {} writing new one'.format(blacklistfile))
+            bl = pd.DataFrame(columns = ['series', 'reason'])
+    else:
+        bl = pd.DataFrame(columns = ['series', 'reason'])
+
     for examdir in examdirs:
-        compare_exam_headers(stdmap, examdir, ignorelist)
-        
-if __name__ == '__main__': 
+        compare_exam_headers(stdmap, examdir, ignorelist, bl)
+
+    ## if blacklistfile was given - write the results out to csv
+    if blacklistfile != None:
+        bl.to_csv(blacklistfile, sep='\s*')
+
+
+
+if __name__ == '__main__':
     main()
