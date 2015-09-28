2239f6394aad2f8fd4caa1d7b94e9ef16972ca19
edickie <erin.w.dickie@gmail.com>
2015-09-28 14:19:50 -0400

    just need to fix copy and paste bit

--- bin/tigr-write-runsh.py
+++ bin/tigr-write-runsh.py
@@ -4,13 +4,13 @@ Reads from yml config file and export info to determine what to put in run.sh fo
 Then writes run.sh file.
 
 Usage:
-    tigr-write-runsh.py <config.yml>
+    tigr-write-runsh.py [options] <config.yml>
 
 Arguments:
     <config.yml>             Configuration file in yml format
 
 Options:
-    --outputfile             Full path to outputfile (default is ${PROJECTDIR}/bin/run.sh)
+    --outputfile FILE        Full path to outputfile (default is ${PROJECTDIR}/bin/run.sh)
     --quiet                  Don't print warnings
     --verbose                Print warnings
     --help                   Print help
@@ -39,7 +39,7 @@ import difflib
 
 arguments       = docopt(__doc__)
 config_yml      = arguments['<config.yml>']
-outputfile      = arguments['<outputfile>']
+outputfile      = arguments['--outputfile']
 VERBOSE         = arguments['--verbose']
 QUIET           = arguments['--quiet']
 
@@ -47,27 +47,42 @@ QUIET           = arguments['--quiet']
 if not os.path.isfile(config_yml):
     sys.exit("configuration file not found. Try again.")
 
+## load the yml file
 with open(config_yml, 'r') as stream:
     config = yaml.load(stream)
 
+## check that the expected keys are there
 ExpectedKeys = ['MRUSER','StudyName','PROJECTDIR','Sites','XNAT_PROJECT']
 diffs = set(ExpectedKeys) - set(config.keys())
 if len(diffs) > 0:
     sys.exit("configuration file missing {}".format(diffs))
 
+## check that the projectdir exists
 projectdir =  os.path.normpath(config['PROJECTDIR'])
 if not os.path.exists(projectdir):
     print("WARNING: PROJECTDIR {} does not exist".format(projectdir))
 
-ExpectedKeys = ['MRUSER','StudyName','PROJECTDIR','Sites','XNAT_PROJECT']
+## read in the site names as a list
+SiteNames = []
+for site in config['Sites']:
+    SiteNames.append(site.keys()[0])
 
+## read export info
+exportinfofile = os.path.join(projectdir,'metadata','exportinfo.csv')
+if not os.path.isfile(exportinfofile):
+    sys.exit("Can't find exportinfofile {}".format(exportinfofile))
+else:
+    exportinfo = pd.read_csv(exportinfofile, sep='\s*',engine='python')
+
+## get ScanTypes from exportinfo 'tags' column
+ScanTypes = exportinfo.tag.tolist()
 
 ## unless an outputfile is specified, set the output to ${PROJECTDIR}/bin/run.sh
 if outputfile == None:
     ouputfile = os.path.join(projectdir,'bin','run.sh')
 
 #open file for writing
-runsh = open(filename,'w')
+runsh = open(outputfile,'w')
 
 runsh.write('#!/bin/bash\n')
 runsh.write('#!/# Runs pipelines like a bro\n#!/#\n')
@@ -81,17 +96,18 @@ runsh.write('#!/#\n#!/#\n')
 
 ## write the top bit
 runsh.write('export STUDYNAME=' + config['StudyName'] + '     # Data archive study name\n')
-runsh.write('export XNAT_PROJECT=' config['XNAT_PROJECT'] + '  # XNAT project name\n')
+runsh.write('export XNAT_PROJECT=' + config['XNAT_PROJECT'] + '  # XNAT project name\n')
 runsh.write('export MRUSER=' + config['MRUSER'] +'         # MR Unit FTP user\n')
 runsh.write('export PROJECTDIR='+ projectdir +'\n')
 
-## write the export info
-for site in config['Sites']:
-    runsh.write('export XNAT_ARCHIVE_' + site + '=')
-    runsh.write(config['Sites'][site]['XNAT_Archive'] + '\n')
+## write the export from XNAT
+for siteinfo in config['Sites']:
+    site = siteinfo.keys()[0]
+    xnat = siteinfo[site]['XNAT_Archive']
+    runsh.write('export XNAT_ARCHIVE_' + site + '=' + xnat + '\n')
 
 ## write the gold standard info
-for site in config['Sites']:
+for site in SiteNames:
     runsh.write('export ' + site + '_STANDARD=')
     runsh.write(projectdir + '/metadata/gold_standards/' + site + '\n')
 
@@ -107,25 +123,25 @@ runsh.write('function message () { [[ "$args" =~ "--quiet" ]] || echo "$(date): 
 
 ## start running stuff
 runsh.write('{\n')
-runsh.write('  message "Running pipelines for study: $STUDYNAME"\n')
+runsh.write('  message "Running pipelines for study: $STUDYNAME"\n\n')
 
 ## get the scans from the camh server
 runsh.write('  message "Get new scans..."\n')
-runsh.write('  dm-sftp-sync.sh ${MRUSER}@mrftp.camhpet.ca "${MRUSER}*/*" ${PROJECTDIR}/data/zips\n')
+runsh.write('  dm-sftp-sync.sh ${MRUSER}@mrftp.camhpet.ca "${MRUSER}*/*" ${PROJECTDIR}/data/zips\n\n')
 
 ## link.py part
 runsh.write('  message "Link scans..."\n')
-runsh.write('  link.py \\n')
-runsh.write('    --lookup=${PROJECTDIR}/metadata/scans.csv \\n')
-runsh.write('    ${PROJECTDIR}/data/dicom/ \\n')
+runsh.write('  link.py \\\n')
+runsh.write('    --lookup=${PROJECTDIR}/metadata/scans.csv \\\n')
+runsh.write('    ${PROJECTDIR}/data/dicom/ \\\n')
 runsh.write('    ${PROJECTDIR}/data/zips/*.zip\n\n')
 
 ## XNAT uploading from our server
 runsh.write('  message "Uploading new scans to XNAT..."\n')
-runsh.write('  dm-xnat-upload.sh \\n')
-runsh.write('    ${XNAT_PROJECT} \\n')
-runsh.write('    ${XNAT_ARCHIVE_CMH} \\n')
-runsh.write('    ${PROJECTDIR}/data/dicom \\n')
+runsh.write('  dm-xnat-upload.sh \\\n')
+runsh.write('    ${XNAT_PROJECT} \\\n')
+runsh.write('    ${XNAT_ARCHIVE_CMH} \\\n')
+runsh.write('    ${PROJECTDIR}/data/dicom \\\n')
 runsh.write('    ${PROJECTDIR}/metadata/xnat-credentials\n\n')
 
 ## load modules for file conversion
@@ -135,7 +151,7 @@ runsh.write('  module load minc-toolkit/1.0.01\n\n')
 
 ## Extracting the scans from XNAT
 runsh.write('  message "Extract new scans from XNAT..."\n')
-for site in config['Sites']:
+for site in SiteNames:
     runsh.write('  xnat-extract.py ' + \
         '--blacklist ${PROJECTDIR}/metadata/blacklist.csv ' + \
         '--datadir ${PROJECTDIR}/data '\
@@ -149,13 +165,13 @@ runsh.write('  module unload minc-toolkit/1.0.01\n\n')
 
 ## do the dicom header check
 runsh.write('\n  message "Checking DICOM headers... "\n')
-for site in config['Sites']:
+for site in SiteNames:
     runsh.write('  dm-check-headers.py ${' + site + '_STANDARD} ' + \
         '${PROJECTDIR}/dcm/SPN01_' + site + '_*\n')
 
 ## do the gradient directions check
 runsh.write('\n  message "Checking gradient directions..."\n')
-for site in config['Sites']:
+for site in SiteNames:
     runsh.write('  dm-check-bvecs.py ${PROJECTDIR} ${' + site + '_STANDARD} ' + site + '\n')
 
 ## load all the pipeline tools
@@ -172,10 +188,14 @@ runsh.write('  message "Generating QC documents..."\n')
 runsh.write('  qc.py --datadir ${PROJECTDIR}/data/ --qcdir ${PROJECTDIR}/qc --dbdir ${PROJECTDIR}/qc\n')
 runsh.write('  qc-report.py ${PROJECTDIR}\n\n')
 
-runsh.write('  message "Updating phantom plots..."\n')
-runsh.write('  qc-phantom.py ${PROJECTDIR} 20 ' + ' '.join(config['Sites']) + '\n')
-runsh.write('  web-build.py ${PROJECTDIR} \n\n')
+if 'qc-phantom.py' in config['PipelineSettings'].keys():
+    runsh.write('  message "Updating phantom plots..."\n')
+    runsh.write('  qc-phantom.py ${PROJECTDIR} ' + \
+        str(config['PipelineSettings']['qc-phantom.py']['NTP']) + ' ' + \
+        config['PipelineSettings']['qc-phantom.py']['Sites'] + '\n')
+    runsh.write('  web-build.py ${PROJECTDIR} \n\n')
 
+## pushing stuff to git hub
 runsh.write('  message "Pushing QC documents to github..."\n')
 runsh.write('  ( # subshell invoked to handle directory change\n')
 runsh.write('    cd ${PROJECTDIR}\n')
@@ -183,43 +203,48 @@ runsh.write('    git add qc/\n')
 runsh.write('    git add metadata/checklist.csv\n')
 runsh.write('    git diff --quiet HEAD || git commit -m "Autoupdating QC documents"\n')
 runsh.write('    git push --quiet\n')
-runsh.write('  )\n')
-runsh.write('\n')
+runsh.write('  )\n\n')
+
+## pushing website ness
 runsh.write('  message "Pushing website data to github..."\n')
 runsh.write('  (  \n')
 runsh.write('    cd ${PROJECTDIR}/website\n')
 runsh.write('    git add .\n')
 runsh.write('    git commit -m "Updating QC plots"\n')
 runsh.write('    git push --quiet\n')
-runsh.write('  )\n')
-runsh.write('\n')
-runsh.write('  message "Split the PDT2 images..."\n')
-runsh.write('  (\n   dm-proc-split-pdt2.py ${PROJECTDIR}/data/nii/*/*_PDT2_*.nii.gz\n  )\n\n')
+runsh.write('  )\n\n')
 
-runsh.write('  message "Running freesurfer..."\n')
-runsh.write('  dm-proc-freesurfer.py ${PROJECTDIR}\n\n')
+if 'PDT2' in ScanTypes:
+    runsh.write('  message "Split the PDT2 images..."\n')
+    runsh.write('  (\n   dm-proc-split-pdt2.py ${PROJECTDIR}/data/nii/*/*_PDT2_*.nii.gz\n  )\n\n')
 
-runsh.write('  message "Analyzing empathic accuracy data..."\n')
-runsh.write('  dm-proc-ea.py ${PROJECTDIR} /scratch/clevis /archive/data-2.0/code/datman/assets/150409-compcor-nonlin-8fwhm.sh ${PROJECTDIR}/metadata/design\n\n')
+if 'DTI60-1000' in ScanTypes:
+    runsh.write('  message "Running dtifit..."\n')
+    runsh.write('  dm-proc-dtifit.py --datadir ${PROJECTDIR}/data/ --outputdir ${PROJECTDIR}/data/dtifit\n\n')
 
-runsh.write('  message "Analyzing imitate observe data..."\n')
-runsh.write('  dm-proc-imob.py ${PROJECTDIR} /scratch/clevis /archive/data-2.0/code/datman/assets/150409-compcor-nonlin-8fwhm.sh ${PROJECTDIR}/metadata/design\n\n')
+    runsh.write('  message "Running ditfit qc..."\n')
+    runsh.write('  dtifit-qc.py --tag DTI60 ${PROJECTDIR}/data/dtifit/\n\n')
 
-runsh.write('  message "Analyzing resting state data..."\n')
-runsh.write('  dm-proc-rest.py ${PROJECTDIR} /scratch/clevis /archive/data-2.0/code/datman/assets/150409-compcor-nonlin-8fwhm.sh ${PROJECTDIR}/metadata/design\n\n')
+    runsh.write('  message "Running enignmaDTI..."\n')
+    runsh.write('  dm-proc-enigmadti.py --calc-all --QC-transfer ${PROJECTDIR}/metadata/checklist.csv ${PROJECTDIR}/data/dtifit ${PROJECTDIR}/data/enigmaDTI\n\n')
 
-runsh.write('  message "Running dtifit..."\n')
-runsh.write('  module load FSL/5.0.7\n')
-runsh.write('  dm-proc-dtifit.py --tag "MRC" --fa_thresh "0.15" --datadir ${PROJECTDIR}/data/ --outputdir ${PROJECTDIR}/data/dtifit\n')
-runsh.write('  dm-proc-dtifit.py --tag "ZHH" --datadir ${PROJECTDIR}/data/ --outputdir ${PROJECTDIR}/data/dtifit\n')
-runsh.write('  dm-proc-dtifit.py --tag "CMH" --datadir ${PROJECTDIR}/data/ --outputdir ${PROJECTDIR}/data/dtifit\n\n')
+if 'T1' in ScanTypes:
+    runsh.write('  message "Running freesurfer..."\n')
+    runsh.write('  dm-proc-freesurfer.py ${PROJECTDIR}\n\n')
 
-runsh.write('  message "Running ditfit qc..."\n')
-runsh.write('  dtifit-qc.py --tag DTI60 ${PROJECTDIR}/data/dtifit/\n\n')
+if 'EMP' in ScanTypes:
+    runsh.write('  message "Analyzing empathic accuracy data..."\n')
+    runsh.write('  dm-proc-ea.py ${PROJECTDIR} /scratch/clevis /archive/data-2.0/code/datman/assets/150409-compcor-nonlin-8fwhm.sh ${PROJECTDIR}/metadata/design\n\n')
 
-runsh.write('  message "Running enignmaDTI..."\n')
-runsh.write('  dm-proc-enigmadti.py --calc-all --QC-transfer ${PROJECTDIR}/metadata/checklist.csv ${PROJECTDIR}/data/dtifit ${PROJECTDIR}/data/enigmaDTI\n\n')
+if ('IMI' in ScanTypes) & ('OBS' in ScanTypes):
+    runsh.write('  message "Analyzing imitate observe data..."\n')
+    runsh.write('  dm-proc-imob.py ${PROJECTDIR} /scratch/clevis /archive/data-2.0/code/datman/assets/150409-compcor-nonlin-8fwhm.sh ${PROJECTDIR}/metadata/design\n\n')
 
+if 'RST' in ScanTypes:
+    runsh.write('  message "Analyzing resting state data..."\n')
+    runsh.write('  dm-proc-rest.py ${PROJECTDIR} /scratch/clevis /archive/data-2.0/code/datman/assets/150409-compcor-nonlin-8fwhm.sh ${PROJECTDIR}/metadata/design\n\n')
+
+### unload all the modules
 runsh.write('  module unload AFNI/2014.12.16 \n')
 runsh.write('  module unload FSL/5.0.7\n')
 runsh.write('  module unload matlab/R2013b_concurrent\n')
@@ -227,5 +252,21 @@ runsh.write('  module unload freesurfer/5.3.0 \n')
 runsh.write('  module unload python/2.7.9-anaconda-2.1.0-150119 \n')
 runsh.write('  module unload python-extras/2.7.9\n\n')
 
+### tee out a log
 runsh.write('  message "Done."\n')
 runsh.write('} | tee -a ${PROJECTDIR}/logs/run-all-${DATESTAMP}.log\n')
+
+## close the file
+runsh.close()
+
+### change anything that needs to be changed with Find and Replace
+if 'FindandReplace' in config.keys():
+    for block in config['FindandReplace']:
+        toFind = block['Find']
+        toReplace = block['Replace']
+        with open (outputfile,'r') as runsh:
+            allrun = runsh.read()
+        if block['Find'] in allrun:
+            allrun.replace(block['Find'],block['Replace'])
+        else:
+            print('WARNING: could not find {} in run.sh file'.format(block['Find']))
