99977027866d5143e86d13ab08083ec3d492e1cf
edickie <erin.w.dickie@gmail.com>
2015-12-09 15:59:25 -0500

    added check for FSL enviroment

--- assets/dtifit.sh
+++ assets/dtifit.sh
@@ -3,7 +3,7 @@ function usage {
 echo "
 Runs eddy_correct, bet and dtifit on a dwi image
 
-Usage: 
+Usage:
    dtifit.sh <dwifile.nii.gz> <outputdir> <ref_vol> <fa_thresh>
 
 "
@@ -15,9 +15,15 @@ outputdir="$2"
 ref_vol="$3"
 fa_thresh="$4"
 
-if [ $# -ne 4 ]; then 
+if [ $# -ne 4 ]; then
   usage;
   exit 1;
+fi
+
+if [ -z "$FSLDIR" ]; then
+    echo "FSLDIR environment variable is undefined. Try again."
+    echo "(ﾉಥ益ಥ）ﾉ ┻━┻"
+    exit 1;
 fi
 
 # input files
@@ -33,17 +39,18 @@ bet=${b0}_bet
 mask=${bet}_mask
 dtifit=${eddy}_dtifit
 
+
 if [ ! -e ${eddy}.nii.gz ]; then
   eddy_correct ${dwifile} ${eddy} ${ref_vol}
 fi
 
 if [ ! -e ${b0}.nii.gz ]; then
   fslroi ${eddy} ${b0} ${ref_vol} 1
-fi 
+fi
 
 if [ ! -e ${bet}.nii.gz ]; then
   bet ${b0} ${bet} -m -f ${fa_thresh} -R
-fi 
+fi
 
 if [ ! -e ${dtifit}_FA.nii.gz ]; then
   dtifit -k ${eddy} -m ${mask} -r ${bvec} -b ${bval} --save_tensor -o ${dtifit}
@@ -51,8 +58,8 @@ fi
 
 if [ ! -e ${eddy}.bvec ]; then
   cp ${bvec} ${eddy}.bvec
-fi 
+fi
 
 if [ ! -e ${eddy}.bval ]; then
   cp ${bval} ${eddy}.bval
-fi 
+fi
