cb15fbea5741cfc8bdf30297b39014f6c5f7c3d4
Jon Pipitone <jon@pipitone.ca>
2016-06-03 10:59:30 -0400

    Adding ninet source code

---
+++ assets/ninet/ninet/__init__.py
@@ -0,0 +1,11 @@
+"""
+NiNet is a set of tools for doing network-analysis on neuroimaging data. 
+
+I'll add more here later, promise.
+"""
+
+from . import stats
+from . import utilities
+from . import signal
+from . import plot
+
