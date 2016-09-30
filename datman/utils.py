ce6055e051b376a63ae6e53c881971fd4ecdb431
Joseph Viviano <joseph@viviano.ca>
2016-09-30 15:39:12 -0400

    moved command line files from assets into bin, removed old config.yml file

--- datman/utils.py
+++ datman/utils.py
@@ -439,11 +439,11 @@ def check_returncode(returncode):
     if returncode != 0:
         raise ValueError
 
-def get_loaded_modules(): 
+def get_loaded_modules():
     """Returns a space separated list of loaded modules
 
     These are modules loaded by the environment-modules system. This function
-    just looks in the LOADEDMODULES environment variable for the list. 
+    just looks in the LOADEDMODULES environment variable for the list.
     """
     return " ".join(os.environ.get("LOADEDMODULES","").split(":"))
 
