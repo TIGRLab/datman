b6f22a3906a324a346f09f67ea5d0c409a8775eb
Tom Wright <tom@maladmin.com>
2016-10-14 12:20:30 -0400

    Added function to get Project name from XNAT ID

--- datman/config.py
+++ datman/config.py
@@ -20,7 +20,6 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARN)
 
 
-
 class config:
     site_config = None
     study_config = None
@@ -42,7 +41,8 @@ class config:
     def load_yaml(self, filename):
         ## Read in the configuration yaml file
         if not os.path.isfile(filename):
-            raise ValueError("configuration file {} not found. Try again.".format(filename))
+            raise ValueError("configuration file {} not found. Try again."
+                             .format(filename))
 
         ## load the yml file
         with open(filename, 'r') as stream:
@@ -52,13 +52,34 @@ class config:
 
     def set_study_config(self, project_name):
         # make the supplied project_name case insensitive
-        valid_projects = {k.lower(): k for k in self.site_config['Projects'].keys()}
+        valid_projects = {k.lower(): k
+                          for k in self.site_config['ProjectSettings'].keys()}
+
         if project_name.lower() in valid_projects.keys():
-            base_path = self.site_config['SystemSettings'][self.system_name]['DATMAN_PROJECTSDIR']
-            project_path = self.site_config['Projects'][valid_projects[project_name.lower()]]
-            project_path = project_path.replace('<DATMAN_PROJECTSDIR>', base_path)
-            self.study_config = self.load_yaml(os.path.join(project_path,
-                                                            'metadata',
-                                                            'project_settings.yml'))
+            project_name = project_name.upper()
+            system_settings = self.site_config['SystemSettings']
+            config_path = system_settings[self.system_name]['CONFIG_DIR']
+
+            project_settings_file = os.path.join(config_path,
+                self.site_config['ProjectSettings'][project_name]['basedir'],
+                self.site_config['ProjectSettings'][project_name]['config_file'])
+
+            self.study_config = self.load_yaml(project_settings_file)
+
+
+def map_xnat_archive_to_project(archive, **kwargs):
+    """Maps the XNAT tag (e.g. SPN01) to the project name e.g. SPINS"""
+    cfg = config(**kwargs)
+
+    for project in cfg.site_config['ProjectSettings'].keys():
+        cfg.set_study_config(project)
+        for site in cfg.study_config['Sites']:
+            if site['XNAT_ID'].lower() == archive.lower():
+                return(project)
+    # didn't find a match throw a worning
+    logger.warn('Failed to find a valid project for xnat id:{}'
+                .format(archive))
+    return
+
 if __name__ == '__main__':
     ARGUMENTS = docopt(__doc__)
