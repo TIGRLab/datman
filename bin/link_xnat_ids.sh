ac3b5ab6543655a3888c9eee87f8d0d477e89a56
root <root@golgi.kimel.ric>
2016-11-10 14:01:08 -0500

    Fixed a bug in link_xnat_ids

--- bin/link_xnat_ids.sh
+++ bin/link_xnat_ids.sh
@@ -96,7 +96,7 @@ transfer_scan_info.py -v ${credentials} 2>&1 | grep "alternate ids" | cut -d " "
       else
 
         # Run with default project config-yaml file
-        LINK_OUTPUT=`dm-link-project-scans.py
+        LINK_OUTPUT=`dm-link-project-scans.py \
                       ${link_file} \
                       ${subject_id} \
                       ${linked_id} 2>&1`
