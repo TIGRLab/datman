17588689b4498515bba06a1b255c0afeacfcaacc
Joseph <joseph@viviano.ca>
2017-04-03 15:03:19 -0400

    doctopt import now points to datman module

--- bin/archive-manifest.py
+++ bin/archive-manifest.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 """
 Prints a list of the series in an MRI exam archive tarball or directory
-   
+
 Usage:
     archive-manifest.py [options] <archive>...
 
-Arguments: 
+Arguments:
     <archive>            Exam archive (zip, tarball, or folder)
 
-Options: 
+Options:
      --headers=LIST      Comma separated list of dicom header names to print.
      --oneseries         Only show one series (useful for just exam info)
      --showheaders       Just list all of the headers for each archive
@@ -25,26 +25,26 @@ import StringIO
 import pandas as pd
 
 default_headers=[
-    'StudyDescription', 
-    'StudyID', 
-    'PatientName', 
+    'StudyDescription',
+    'StudyID',
+    'PatientName',
     'SeriesNumber',
     'SeriesDescription']
 
-def main(): 
-    from docopt import docopt
+def main():
+    from datman.docopt import docopt
     import sys
     arguments = docopt(__doc__)
 
     if arguments['--showheaders']:
         for archive in arguments['<archive>']:
-            manifest = datman.utils.get_archive_headers(archive, 
+            manifest = datman.utils.get_archive_headers(archive,
                                                         stop_after_first=False)
-            filepath, headers = manifest.items()[0] 
+            filepath, headers = manifest.items()[0]
             print ",".join([archive,filepath])
             print "\t"+"\n\t".join(headers.dir())
         return
-        
+
     headers = arguments['--headers'] and arguments['--headers'].split(',') or \
                 default_headers[:]
     headers.insert(0,"Path")
@@ -52,16 +52,16 @@ def main():
     rows = []
     for archive in arguments['<archive>']:
         manifest = datman.utils.get_archive_headers(archive)
-        sortedseries = sorted(manifest.iteritems(), 
+        sortedseries = sorted(manifest.iteritems(),
                               key = lambda x: x[1].get('SeriesNumber'))
         for path, dataset in sortedseries:
             row = dict([(header,dataset.get(header,"")) for header in headers])
-            row['Path'] = path 
+            row['Path'] = path
             rows.append(row)
-            if arguments['--oneseries']: break 
-    
+            if arguments['--oneseries']: break
+
     data = pd.DataFrame(rows)
     print data.to_csv(index=False)
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     main()
