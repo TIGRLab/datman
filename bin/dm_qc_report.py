201500fd29c449838ef48cc2f4dff039c0521190
Michael Joseph <josephmje.22@gmail.com>
2018-08-20 15:19:13 -0400

    get scanlength of all files

--- bin/dm_qc_report.py
+++ bin/dm_qc_report.py
@@ -163,7 +163,7 @@ def gather_input_req(nifti, pipeline):
             'anat'      :   ['qc-adni',             basename + '.nii.gz'],
             'fmri'      :   ['qc-fbirn-fmri',       basename + '.nii.gz'],
             'dti'       :   ['qc-fbirn-dti',        basename + '.nii.gz', basename + '.bvec', basename + '.bval'],
-            'qa_dti'    :   ['qa-dti',              basename + '.nii.gz', basename + '.bvec', basename + '.bval', 
+            'qa_dti'    :   ['qa-dti',              basename + '.nii.gz', basename + '.bvec', basename + '.bval',
                 '--accel' if 'NO' in basename else ''],
             'abcd_fmri' :   ['qc_abcd_fmri',        basename + '.nii.gz', dcm, basename + '.json']
             }
@@ -621,7 +621,7 @@ def write_table(report, exportinfo, subject):
         try:
             data = nib.load(scan_nii_path)
             try:
-                scanlength = data.shape[3]
+                scanlength = data.shape[-1]
             except:
                 #Note: this might be expected, e.g., for a T1
                 logging.debug("{} exists but scanlength cannot be read.".format(scan_nii_path))
