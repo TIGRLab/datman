4c178c2b6e0fefac1098399939847d402314e00a
Jerry Jeyachandra <jjeyachandra@tesla.camhres.ca>
2018-08-28 12:24:52 -0400

    remove sub- prefix, syntax fix, tested to run

--- bin/dm_bids_app.py
+++ bin/dm_bids_app.py
@@ -255,7 +255,7 @@ def get_init_cmd(study,sgroup,tmp_dir,out_dir,simg,log_tag):
 
     '''.format(home=os.path.join(tmp_dir,'home.XXXXX'),
             simg=simg,
-            sub=get_bids_name(sgroup.replace('sub-','')),
+            sub=get_bids_name(sgroup).replace('sub-',''),
             out=out_dir,
             log_tag=log_tag)
 
