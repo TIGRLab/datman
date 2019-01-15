e0a58087302044a4ba6ac1dab0e11ece5755160d
Dawn E. Smith <Dawn.Smith@camh.ca>
2019-01-15 14:48:28 -0500

    Added redcap monitor when new sessions added

--- datman/dashboard.py
+++ datman/dashboard.py
@@ -10,7 +10,7 @@ from datman.exceptions import DashboardException
 logger = logging.getLogger(__name__)
 
 try:
-    from dashboard import queries
+    from dashboard import queries, monitors
 except ImportError:
     dash_found = False
     logger.error("Dashboard not found, proceeding without it.")
@@ -164,7 +164,12 @@ def add_session(name, date=None):
                     "".format(date))
             raise DashboardException("Invalid date format {}".format(date))
 
-    return timepoint.add_session(sess_num, date=date)
+    new_session = timepoint.add_session(sess_num, date=date)
+
+    if timepoint.expects_redcap():
+        monitors.monitor_redcap_import(str(timepoint), sess_num)
+
+    return new_session
 
 
 @dashboard_required
