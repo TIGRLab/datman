e802129d6da009ed583b2890a653923bc5902b7d
Mathu Manogaran <Mathuvanthi.Manogaran@camh.ca>
2019-06-04 12:05:43 -0400

    Added way to update study status in the dashboard based on the settings YAML key IsOpen. Fixed bug in config's get_key function where a value=False would cause the function to fail.

---
+++ bin/dm_update_study_status.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import datman.dashboard as dashboard
+import datman.config as cfg
+
+def main():
+    config = cfg.config()
+    studies = config.get_key('Projects').keys()
+
+    for study in studies:
+        try:
+            config.set_study(study)
+        except:
+            pass
+        is_open = config.get_key('IsOpen')
+
+        dashboard.set_study_status(study, is_open)
+
+
+if __name__ == '__main__':
+    main()
