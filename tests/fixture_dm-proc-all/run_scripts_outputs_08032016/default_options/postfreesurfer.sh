bdffaa6529ea7851aa5f9679ca038d0cabefbb53
Dawn E. Smith <Dawn.Smith@camh.ca>
2019-12-11 19:09:18 -0500

    Cleaned legacy code from datman + tests folders. Renamed a few test scripts that weren't snake case

--- tests/fixture_dm-proc-all/run_scripts_outputs_08032016/default_options/postfreesurfer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export SUBJECTS_DIR=tests/fixture_dm-proc-all/run_scripts_outputs_08032016/default_options
-
-## Prints loaded modules to the log
-module list
-
-ENGIMA_ExtractCortical.sh ${SUBJECTS_DIR} STU
-ENGIMA_ExtractSubcortical.sh ${SUBJECTS_DIR} STU
