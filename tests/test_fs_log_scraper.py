4ab30f13f86c5264e573b0742fefdc9b6b588ae6
Dawn E. Smith <Dawn.Smith@camh.ca>
2019-12-17 13:01:53 -0500

    Updated test_fs_log_scraper for pep8

--- tests/test_fs_log_scraper.py
+++ tests/test_fs_log_scraper.py
@@ -9,6 +9,7 @@ import datman.fs_log_scraper as scraper
 
 logging.disable(logging.CRITICAL)
 
+
 class TestFSLog(unittest.TestCase):
 
     fs_path = '/some/path/freesurfer/subject_id'
@@ -16,8 +17,10 @@ class TestFSLog(unittest.TestCase):
 
     @patch('datman.fs_log_scraper.FSLog.read_log')
     @patch('glob.glob')
-    def test_sets_running_status_when_subject_running_less_than_24h(self,
-            mock_glob, mock_read_log):
+    def test_sets_running_status_when_subject_running_less_than_24h(
+                self,
+                mock_glob,
+                mock_read_log):
         mock_glob.return_value = ['/some/path/IsRunning.lh+rh']
         # Time zone isnt taken into account when time is compared, but required
         # by the date parsing code
@@ -30,8 +33,10 @@ class TestFSLog(unittest.TestCase):
 
     @patch('datman.fs_log_scraper.FSLog.read_log')
     @patch('glob.glob')
-    def test_sets_status_timedout_when_subject_running_more_than_24h(self,
-            mock_glob, mock_read_log):
+    def test_sets_status_timedout_when_subject_running_more_than_24h(
+                self,
+                mock_glob,
+                mock_read_log):
         mock_glob.return_value = ['/some/path/IsRunning.lh+rh']
         two_days_ago = datetime.datetime.now(tz=EST()) - datetime.timedelta(2)
         mock_read_log.return_value = ['DATE {}'.format(
@@ -39,11 +44,12 @@ class TestFSLog(unittest.TestCase):
 
         my_log = scraper.FSLog(self.fs_path)
 
-        assert my_log.status == scraper.FSLog._TIMEDOUT.format('IsRunning.lh+rh')
+        assert my_log.status == scraper.FSLog._TIMEDOUT.format(
+                'IsRunning.lh+rh')
 
     @patch('glob.glob')
     def test_sets_status_maybe_halted_when_IsRunning_log_unreadable(self,
-            mock_glob):
+                                                                    mock_glob):
         # Return path to nonexistent file
         mock_glob.return_value = ["/some/path/IsRunning.lh+rh"]
 
@@ -71,8 +77,9 @@ class TestFSLog(unittest.TestCase):
 
     @patch('datman.fs_log_scraper.FSLog.read_log')
     @patch('os.path.exists')
-    def test_values_set_to_empty_when_recon_done_unreadable(self, mock_exists,
-            mock_read_log):
+    def test_values_set_to_empty_when_recon_done_unreadable(self,
+                                                            mock_exists,
+                                                            mock_read_log):
         recon_done = self.fs_path + '/scripts/recon-all.done'
         mock_exists.side_effect = lambda x: True if x == recon_done else False
         mock_read_log.return_value = {}
@@ -91,7 +98,8 @@ class TestFSLog(unittest.TestCase):
 
     def test_args_excludes_subject_specific_arguments(self):
         cmd_args = '-all -qcache -notal-check -parallel -subjid SOMEID12345 ' \
-                + '-i /some/path/nii_input1.nii.gz -T2 /some/path/nii_input2.nii.gz'
+                + '-i /some/path/nii_input1.nii.gz ' \
+                + '-T2 /some/path/nii_input2.nii.gz'
         relevant_args = ['-all', '-qcache', '-notal-check', '-parallel']
         expected = " ".join(sorted(relevant_args))
 
@@ -112,8 +120,9 @@ class TestFSLog(unittest.TestCase):
     def test_nii_inputs_includes_T2_inputs(self):
         t1 = '/some/path/nii_input1.nii.gz'
         t2 = 'some/path/nii_input2.nii.gz'
-        cmd_args = '-all -qcache -notal-check -nuiterations 8 -subjid SOMEID12345' \
-                + '-i {} -T2 {}'.format(t1, t2)
+        cmd_args = '-all -qcache -notal-check -nuiterations 8 ' \
+                   + '-subjid SOMEID12345' \
+                   + '-i {} -T2 {}'.format(t1, t2)
 
         nii_inputs = scraper.FSLog.get_niftis(cmd_args)
 
@@ -122,14 +131,16 @@ class TestFSLog(unittest.TestCase):
     def test_nii_inputs_doesnt_include_args(self):
         t1 = '/some/path/nii_input1.nii.gz'
         t2 = 'some/path/nii_input2.nii.gz'
-        cmd_args = '-all -qcache -notal-check -nuiterations 8 -subjid SOMEID12345' \
-                + '-i {} -T2 {}'.format(t1, t2)
+        cmd_args = '-all -qcache -notal-check -nuiterations 8 ' \
+                   + '-subjid SOMEID12345' \
+                   + '-i {} -T2 {}'.format(t1, t2)
 
         nii_inputs = scraper.FSLog.get_niftis(cmd_args)
 
         for arg in ['-all', '-qcache', '-notal-check', '-nuiterations 8',
-                '-subjid SOMEID12345']:
+                    '-subjid SOMEID12345']:
             assert arg not in nii_inputs
+
 
 class TestChooseStandardSub(unittest.TestCase):
 
@@ -142,7 +153,8 @@ class TestChooseStandardSub(unittest.TestCase):
         subject2 = LogStub('SUBJECT2222', '')
         subject3 = LogStub('SUBJECT3333', 'Still Running')
 
-        standard_sub = scraper.choose_standard_sub([subject1, subject2, subject3])
+        standard_sub = scraper.choose_standard_sub([subject1, subject2,
+                                                    subject3])
 
         assert standard_sub.subject == subject2.subject
 
@@ -156,6 +168,7 @@ class TestChooseStandardSub(unittest.TestCase):
         subject2 = LogStub('SUBJECT2', 'Still Running')
 
         standard_sub = scraper.choose_standard_sub([subject1, subject2])
+
 
 class TestVerifyStandards(unittest.TestCase):
 
@@ -178,6 +191,7 @@ class TestVerifyStandards(unittest.TestCase):
 
         # Function always passes if extra key doesnt cause exception
         assert True
+
 
 class CheckDiff(unittest.TestCase):
 
@@ -204,6 +218,7 @@ class CheckDiff(unittest.TestCase):
         diffs = scraper.check_diff(subject_args, standard_args)
 
         assert isinstance(diffs, str)
+
 
 # A timezone class, to fill in the expected (but not used) datetime timezone
 class EST(datetime.tzinfo):
