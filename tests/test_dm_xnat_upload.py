ec5a5814d474e2c6ebf27f33d671ffb062c2e570
Dawn E. Smith <Dawn.Smith@camh.ca>
2019-12-17 16:18:07 -0500

    Removed no longer relevant test from test_dm_xnat_upload

--- tests/test_dm_xnat_upload.py
+++ tests/test_dm_xnat_upload.py
@@ -36,13 +36,13 @@ class CheckFilesExist(unittest.TestCase):
             '1.2.840.113619.2.5.18242516414121059301412105930313000',
             '1.2.840.113619.2.80.142631515.25030.1412106138.1.0.2']
     archive_experiment_id = '1.2.840.113619.6.336.' \
-                             '254801968553430904107911738210738061468'
+                            '254801968553430904107911738210738061468'
 
     @raises(Exception)
     @patch('bin.dm_xnat_upload.missing_resource_data')
     @patch('datman.utils.get_archive_headers')
     def test_raises_exception_if_scan_uids_mismatch(self, mock_headers,
-                mock_missing_resources):
+                                                    mock_missing_resources):
         # Set up
         mock_headers.return_value = self.__generate_mock_headers(bad_id=True)
         mock_missing_resources.return_value = False
@@ -54,10 +54,6 @@ class CheckFilesExist(unittest.TestCase):
 
         # Should raise an exception, so assertion is never reached
         assert False
-
-##### To do:
-    # Test that false is returned when a resource is missing, or when a scan is
-    # missing
 
     def __generate_mock_headers(self, bad_id=False):
         headers = {}
@@ -76,33 +72,3 @@ class CheckFilesExist(unittest.TestCase):
         with open(text_file, 'r') as session_data:
             xnat_session = eval(session_data.read())
         return xnat_session
-
-class GetResources(unittest.TestCase):
-    name_list = ['some_zipfile_name/',
-                 'some_zipfile_name/dicom_file1.dcm',
-                 'some_zipfile_name/dicom_file2.dcm',
-                 'some_zipfile_name/bvals.txt',
-                 'some_zipfile_name/gradOrs.txt',
-                 'some_zipfile_name/dicom_file3.dcm',
-                 'some_zipfile_name/Name_info.txt',
-                 'some_zipfile_name/subjectid_EmpAcc.log']
-
-    @patch('bin.dm_xnat_upload.is_dicom')
-    @patch('io.BytesIO')
-    def test_returns_all_resources(self, mock_IO, mock_isdicom):
-        # Set up inputs
-        archive_zip = MagicMock(spec=zipfile.ZipFile)
-        archive_zip.return_value.namelist.return_value = self.name_list
-        expected_resources = ['some_zipfile_name/bvals.txt',
-                              'some_zipfile_name/gradOrs.txt',
-                              'some_zipfile_name/Name_info.txt',
-                              'some_zipfile_name/subjectid_EmpAcc.log']
-
-        # Stop get_resources from verifying 'dicoms' in the mock zipfile
-        archive_zip.return_value.read.side_effect = lambda x: x
-        mock_IO.side_effect = lambda x: x
-        mock_isdicom.side_effect = lambda x: True if '.dcm' in x else False
-
-        actual_resources = upload.get_resources(archive_zip.return_value)
-
-        assert sorted(actual_resources) == sorted(expected_resources)
