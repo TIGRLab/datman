2be3f1a3a87eb8395a90a06d2b2a951508bc808b
mjoseph <josephmje.22@gmail.com>
2020-01-02 17:41:07 -0500

    run black

--- datman/__init__.py
+++ datman/__init__.py
@@ -8,6 +8,7 @@ from .__about__ import (
 )
 
 import datman.scanid
+
 # If you remove this and dont manually import config before importing
 # datman/dashboard.py you will get circular import errors. I am sooo sorry.
 # We'd have to store hardcoded file paths in the dashboard database to
@@ -16,12 +17,16 @@ import datman.config
 
 
 __all__ = [
-    '__version__',
-    '__copyright__',
-    '__credits__',
-    '__packagename__',
+    "__version__",
+    "__copyright__",
+    "__credits__",
+    "__packagename__",
 ]
 
-_warnings.filterwarnings('ignore', r'This has not been fully tested. Please report any failures.')
-_warnings.filterwarnings('ignore', r"can't resolve package from __spec__ or __package__")
-_warnings.simplefilter('ignore', DeprecationWarning)
+_warnings.filterwarnings(
+    "ignore", r"This has not been fully tested. Please report any failures."
+)
+_warnings.filterwarnings(
+    "ignore", r"can't resolve package from __spec__ or __package__"
+)
+_warnings.simplefilter("ignore", DeprecationWarning)
--- get_version.py
+++ get_version.py
@@ -8,10 +8,11 @@ import os.path as op
 
 def main():
     """Print current datman version."""
-    sys.path.insert(0, op.abspath('.'))
+    sys.path.insert(0, op.abspath("."))
     from datman.__about__ import __version__
+
     print(__version__)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
