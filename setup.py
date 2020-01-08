d1f39d6d078821f3ed8a0557f903cedce88c9f47
mjoseph <josephmje.22@gmail.com>
2020-01-07 19:24:47 -0500

    update project description

--- setup.py
+++ setup.py
@@ -3,10 +3,6 @@ import sys
 from setuptools import setup
 import versioneer
 
-# Give setuptools a hint to complain if it's too old a version
-# 30.3.0 allows us to put most metadata in setup.cfg
-# Should match pyproject.toml
-# Not going to help us much without numpy or new pip, but gives us a shot
 SETUP_REQUIRES = ["setuptools >= 40.8"]
 # This enables setuptools to install wheel on-the-fly
 SETUP_REQUIRES += ["wheel"] if "bdist_wheel" in sys.argv else []
