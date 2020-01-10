3105d2aac3a4e78937d6d33e017bb088e6e2c8b9
Jerry Jeyachandra <jerrold.jeyachandra@gmail.com>
2020-01-10 15:10:48 -0500

    fixed bug where test unconditionally passes

--- tests/test_dm_xnat_upload.py
+++ tests/test_dm_xnat_upload.py
@@ -2,7 +2,7 @@ import unittest
 import importlib
 import logging
 
-from nose.tools import raises
+import pytest
 from mock import patch, MagicMock
 
 import datman
@@ -23,34 +23,36 @@ class CheckFilesExist(unittest.TestCase):
     session_no_resources = FIXTURE + "xnat_session_missing_resources.txt"
     session_missing_data = FIXTURE + "xnat_session_missing_scan_data.txt"
     archive_scan_uids = [
-            '1.2.840.113619.2.336.4120.8413787.19465.1412083372.445',
-            '1.2.840.113619.2.336.4120.8413787.19465.1412083372.444',
-            '1.2.840.113619.2.336.4120.8413787.19465.1412083372.447',
-            '1.2.840.113619.2.336.4120.8413787.19465.1412083372.446',
-            '1.2.840.113619.2.336.4120.8413787.19465.1412083372.440',
-            '1.2.840.113619.2.80.142631515.25030.1412106144.3.0.2',
-            '1.2.840.113619.2.336.4120.8413787.19465.1412083372.443',
-            '1.2.840.113619.2.336.4120.8413787.19465.1412083372.442',
-            '1.2.840.113619.2.5.18242516414121059301412105930313000',
-            '1.2.840.113619.2.80.142631515.25030.1412106138.1.0.2']
+        '1.2.840.113619.2.336.4120.8413787.19465.1412083372.445',
+        '1.2.840.113619.2.336.4120.8413787.19465.1412083372.444',
+        '1.2.840.113619.2.336.4120.8413787.19465.1412083372.447',
+        '1.2.840.113619.2.336.4120.8413787.19465.1412083372.446',
+        '1.2.840.113619.2.336.4120.8413787.19465.1412083372.440',
+        '1.2.840.113619.2.80.142631515.25030.1412106144.3.0.2',
+        '1.2.840.113619.2.336.4120.8413787.19465.1412083372.443',
+        '1.2.840.113619.2.336.4120.8413787.19465.1412083372.442',
+        '1.2.840.113619.2.5.18242516414121059301412105930313000',
+        '1.2.840.113619.2.80.142631515.25030.1412106138.1.0.2'
+    ]
     archive_experiment_id = '1.2.840.113619.6.336.' \
                             '254801968553430904107911738210738061468'
 
-    @raises(Exception)
-    @patch('bin.dm_xnat_upload.missing_resource_data')
+    @patch('bin.dm_xnat_upload.resource_data_exists')
     @patch('datman.utils.get_archive_headers')
     def test_raises_exception_if_scan_uids_mismatch(self, mock_headers,
-                                                    mock_missing_resources):
+                                                    mock_resources_exist):
         # Set up
         mock_headers.return_value = self.__generate_mock_headers(bad_id=True)
-        mock_missing_resources.return_value = False
+        mock_resources_exist.return_value = True
         xnat_session = self.__get_xnat_session(self.session)
 
         # Run
-        upload.check_files_exist(self.archive, xnat_session, self.ident)
+        data_exists, resources_exist = upload.check_files_exist(
+            self.archive, xnat_session)
 
         # Should raise an exception, so assertion is never reached
-        assert False
+        assert data_exists
+        assert resources_exist
 
     def __generate_mock_headers(self, bad_id=False):
         headers = {}
@@ -68,4 +70,4 @@ class CheckFilesExist(unittest.TestCase):
     def __get_xnat_session(self, text_file):
         with open(text_file, 'r') as session_data:
             xnat_session = eval(session_data.read())
-        return xnat_session
+        return datman.xnat.Session(xnat_session)
