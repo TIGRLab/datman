8d13f8461948ad5f555179764708184c65a5f226
Dawn E. Smith <Dawn.Smith@camh.ca>
2020-02-28 17:41:14 -0500

    Refactored session/experiment/scan to allow for multiple experiments

--- datman/xnat.py
+++ datman/xnat.py
@@ -8,6 +8,7 @@ import urllib.parse
 import getpass
 import re
 from xml.etree import ElementTree
+from abc import ABC
 
 import requests
 
@@ -214,7 +215,7 @@ class xnat(object):
                             study)
             raise XnatException(msg)
 
-        return Session(session_json)
+        return XNATSession(session_json)
 
     def make_session(self, study, session):
         url = "{server}/REST/projects/{project}/subjects/{subject}"
@@ -835,20 +836,50 @@ class xnat(object):
             response.raise_for_status()
 
 
-class Session(object):
+class XNATObject(ABC):
+    def _get_field(self, key):
+        if not self.raw_json['data_fields']:
+            return ''
+        return self.raw_json['data_fields'].get(key, '')
 
-    raw_json = None
+
+class XNATSession(XNATObject):
 
     def __init__(self, session_json):
-        # Session attributes
         self.raw_json = session_json
-        self.name = session_json['data_fields']['label']
-        self.project = session_json['data_fields']['project']
+        self.name = self._get_field('label')
+        self.project = self._get_field('project')
+        self.experiments = self._get_experiments()
 
-        # Experiment attributes
-        self.experiment = self._get_experiment()
-        self.experiment_label = self._get_experiment_label()
-        self.experiment_UID = self._get_experiment_UID()
+    def _get_experiments(self):
+        experiments = [exp for exp in self.raw_json['children']
+                       if exp['field'] == 'experiments/experiment']
+
+        if not experiments:
+            logger.debug("No experiments found for {}".format(self.name))
+            return {}
+
+        found = {}
+        for item in experiments[0]['items']:
+            exper = XNATExperiment(self.name, item)
+            found[exper.name] = exper
+
+        return found
+
+    def __str__(self):
+        return "<XNATSession {}>".format(self.name)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class XNATExperiment(XNATObject):
+
+    def __init__(self, session_name, experiment_json):
+        self.raw_json = experiment_json
+        self.session = session_name
+        self.uid = self._get_field('UID')
+        self.name = self._get_field('label')
 
         # Scan attributes
         self.scans = self._get_scans()
@@ -856,70 +887,27 @@ class Session(object):
         self.scan_resource_IDs = self._get_scan_rIDs()
 
         # Resource attributes
-        self.resource_files = self._get_experiment_contents(
-                                            'resources/resource')
+        self.resource_files = self._get_contents('resources/resource')
         self.resource_IDs = self._get_resource_IDs()
 
-        # Misc - basically just OPT CU1 needs this
-        self.misc_resource_IDs = self._get_other_resource_IDs()
+        # # Misc - basically just OPT CU1 needs this
+        # self.misc_resource_IDs = self._get_other_resource_IDs()
 
-    def _get_experiment(self):
-        experiments = [exp for exp in self.raw_json['children']
-                       if exp['field'] == 'experiments/experiment']
-
-        if not experiments:
-            logger.debug("No experiments found for {}".format(self.name))
-            return {}
-        elif len(experiments) > 1:
-            logger.error("More than one session uploaded to ID {}. Processing "
-                         "only the first.".format(self.name))
-
-        return experiments[0]['items'][0]
-
-    def _get_experiment_label(self):
-        if not self.experiment:
-            return ''
-        try:
-            label = self.experiment['data_fields']['label']
-        except KeyError:
-            logger.error("Could not retrieve experiment label for {}".format(
-                    self.name))
-            label = ''
-        return label
-
-    def _get_experiment_UID(self):
-        if not self.experiment:
-            return ''
-        try:
-            uid = self.experiment['data_fields']['UID']
-        except KeyError:
-            uid = ''
-        return uid
-
-    def _get_experiment_contents(self, field):
-        """
-        Retrieves part of the contents of an experiment based on 'field'.
-        e.g. 'scans/scan' or 'resources/resource'
-        """
-        try:
-            children = self.experiment['children']
-        except KeyError:
-            # Nothing uploaded for this experiment
-            children = []
+    def _get_contents(self, data_type):
+        children = self.raw_json.get('children', [])
 
         contents = [child['items'] for child in children
-                    if child['field'] == field]
+                    if child['field'] == data_type]
         return contents
 
     def _get_scans(self):
-        scans = self._get_experiment_contents('scans/scan')
+        scans = self._get_contents('scans/scan')
         if not scans:
-            logger.debug("No scans found for session {}".format(self.name))
+            logger.debug("No scans found for experiment {}".format(self.name))
             return scans
         xnat_scans = []
         for scan_json in scans[0]:
-            xnat_scans.append(XNATScan(self.name, self.experiment_label,
-                                       self.project, scan_json))
+            xnat_scans.append(XNATSeries(self.name, scan_json))
         return xnat_scans
 
     def _get_scan_UIDs(self):
@@ -949,153 +937,147 @@ class Session(object):
 
         resource_ids = {}
         for resource in self.resource_files[0]:
-            try:
-                label = resource['data_fields']['label']
-            except KeyError:
-                label = 'No Label'
+            label = resource['data_fields'].get('label', 'No Label')
             resource_ids[label] = str(resource['data_fields'][
                                                 'xnat_abstractresource_id'])
         return resource_ids
 
-    def _get_other_resource_IDs(self):
-        """
-        OPT's CU site uploads niftis to their server. These niftis are neither
-        classified as resources nor as scans so our code misses them entirely.
-        This functions grabs the abstractresource_id for these and
-        any other unique files aside from snapshots so they can be downloaded
-        """
-        r_ids = []
-        for scan in self.scans:
-            for child in scan.raw_json['children']:
-                for file_upload in child['items']:
-                    data_fields = file_upload['data_fields']
-                    try:
-                        label = data_fields['label']
-                    except KeyError:
-                        # Some entries dont have labels. Only hold some header
-                        # values. These are safe to ignore
-                        continue
+    # def _get_other_resource_IDs(self):
+    #     """
+    #     OPT's CU site uploads niftis to their server. These niftis are neither
+    #     classified as resources nor as scans so our code misses them entirely.
+    #     This functions grabs the abstractresource_id for these and
+    #     any other unique files aside from snapshots so they can be downloaded
+    #     """
+    #     r_ids = []
+    #     for scan in self.scans:
+    #         for child in scan.raw_json['children']:
+    #             for file_upload in child['items']:
+    #                 data_fields = file_upload['data_fields']
+    #                 try:
+    #                     label = data_fields['label']
+    #                 except KeyError:
+    #                     # Some entries dont have labels. Only hold some header
+    #                     # values. These are safe to ignore
+    #                     continue
 
-                    try:
-                        data_format = data_fields['format']
-                    except KeyError:
-                        # Some entries have labels but no format... or neither
-                        if not label:
-                            # If neither, ignore. Should just be an entry
-                            # containing scan parameters, etc.
-                            continue
-                        data_format = label
+    #                 try:
+    #                     data_format = data_fields['format']
+    #                 except KeyError:
+    #                     # Some entries have labels but no format... or neither
+    #                     if not label:
+    #                         # If neither, ignore. Should just be an entry
+    #                         # containing scan parameters, etc.
+    #                         continue
+    #                     data_format = label
 
-                    try:
-                        r_id = str(data_fields['xnat_abstractresource_id'])
-                    except KeyError:
-                        # Some entries have labels and/or a format but no
-                        # actual files and so no resource id. These can also be
-                        # safely ignored.
-                        continue
+    #                 try:
+    #                     r_id = str(data_fields['xnat_abstractresource_id'])
+    #                 except KeyError:
+    #                     # Some entries have labels and/or a format but no
+    #                     # actual files and so no resource id. These can also be
+    #                     # safely ignored.
+    #                     continue
 
-                    # ignore DICOM, it's grabbed elsewhere. Ignore snapshots
-                    # entirely. Some things may not be labelled DICOM but may
-                    # be format 'DICOM' so that needs to be checked for too.
-                    if label != 'DICOM' and (data_format != 'DICOM' and
-                                             label != 'SNAPSHOTS'):
-                        r_ids.append(r_id)
-        return r_ids
+    #                 # ignore DICOM, it's grabbed elsewhere. Ignore snapshots
+    #                 # entirely. Some things may not be labelled DICOM but may
+    #                 # be format 'DICOM' so that needs to be checked for too.
+    #                 if label != 'DICOM' and (data_format != 'DICOM' and
+    #                                          label != 'SNAPSHOTS'):
+    #                     r_ids.append(r_id)
+    #     return r_ids
 
-    def get_resources(self, xnat_connection):
-        """
-        Returns a list of all resource URIs from this session.
-        """
-        resources = []
-        resource_ids = list(self.resource_IDs.values())
-        resource_ids.extend(self.misc_resource_IDs)
-        for r_id in resource_ids:
-            resource_list = xnat_connection.get_resource_list(
-                                    self.project,
-                                    self.name,
-                                    self.experiment_label,
-                                    r_id)
-            resources.extend([item['URI'] for item in resource_list])
-        return resources
+    # def get_resources(self, xnat_connection):
+    #     """
+    #     Returns a list of all resource URIs from this session.
+    #     """
+    #     resources = []
+    #     resource_ids = list(self.resource_IDs.values())
+    #     resource_ids.extend(self.misc_resource_IDs)
+    #     for r_id in resource_ids:
+    #         resource_list = xnat_connection.get_resource_list(
+    #                                 self.project,
+    #                                 self.name,
+    #                                 self.experiment_label,
+    #                                 r_id)
+    #         resources.extend([item['URI'] for item in resource_list])
+    #     return resources
 
-    def download(self, xnat, dest_folder, zip_name=None):
-        """
-        Download a zip file containing all data for this session. Returns the
-        path to the new file if download is successful, raises an exception if
-        not
+    # def download(self, xnat, dest_folder, zip_name=None):
+    #     """
+    #     Download a zip file containing all data for this session. Returns the
+    #     path to the new file if download is successful, raises an exception if
+    #     not
 
-        xnat                An instance of datman.xnat.xnat()
-        dest_folder         The absolute path to the folder where the zip
-                            should be deposited
-        zip_name            An optional name for the output zip file. If not
-                            set the zip name will be session.name
-        """
-        if not self.experiment:
-            raise ValueError("No data found for {}".format(self.name))
+    #     xnat                An instance of datman.xnat.xnat()
+    #     dest_folder         The absolute path to the folder where the zip
+    #                         should be deposited
+    #     zip_name            An optional name for the output zip file. If not
+    #                         set the zip name will be session.name
+    #     """
+    #     if not self.experiment:
+    #         raise ValueError("No data found for {}".format(self.name))
 
-        try:
-            experiment_ID = self.experiment['data_fields']['ID']
-        except KeyError:
-            raise KeyError("Can't retrieve experiment ID for experiment {}. "
-                           "Please check contents.".format(
-                                                    self.experiment_label))
+    #     try:
+    #         experiment_ID = self.experiment['data_fields']['ID']
+    #     except KeyError:
+    #         raise KeyError("Can't retrieve experiment ID for experiment {}. "
+    #                        "Please check contents.".format(
+    #                                                 self.experiment_label))
 
-        # Grab dicoms
-        resources_list = self.scan_resource_IDs
-        # Grab what we define as resources (i.e. tech notes, non-dicoms)
-        resources_list.extend(list(self.resource_IDs.values()))
-        # Grab anything else other than snapshots (i.e. 'MUX' for OPT CU1)
-        resources_list.extend(self.misc_resource_IDs)
+    #     # Grab dicoms
+    #     resources_list = self.scan_resource_IDs
+    #     # Grab what we define as resources (i.e. tech notes, non-dicoms)
+    #     resources_list.extend(list(self.resource_IDs.values()))
+    #     # Grab anything else other than snapshots (i.e. 'MUX' for OPT CU1)
+    #     resources_list.extend(self.misc_resource_IDs)
 
-        if not resources_list:
-            raise ValueError("No scans or resources found for {}"
-                             "".format(self.name))
+    #     if not resources_list:
+    #         raise ValueError("No scans or resources found for {}"
+    #                          "".format(self.name))
 
-        url = ("{}/REST/experiments/{}/resources/{}/files"
-               "?structure=improved&all=true&format=zip".format(
-                            xnat.server,
-                            experiment_ID,
-                            ",".join(resources_list)))
+    #     url = ("{}/REST/experiments/{}/resources/{}/files"
+    #            "?structure=improved&all=true&format=zip".format(
+    #                         xnat.server,
+    #                         experiment_ID,
+    #                         ",".join(resources_list)))
 
-        if not zip_name:
-            zip_name = self.name.upper() + ".zip"
+    #     if not zip_name:
+    #         zip_name = self.name.upper() + ".zip"
 
-        output_path = os.path.join(dest_folder, zip_name)
-        if os.path.exists(output_path):
-            logger.error("Cannot download {}, file already exists.".format(
-                    output_path))
-            return output_path
+    #     output_path = os.path.join(dest_folder, zip_name)
+    #     if os.path.exists(output_path):
+    #         logger.error("Cannot download {}, file already exists.".format(
+    #                 output_path))
+    #         return output_path
 
-        xnat._get_xnat_stream(url, output_path)
+    #     xnat._get_xnat_stream(url, output_path)
 
-        return output_path
+    #     return output_path
 
     def __str__(self):
-        return "<XNATSession {}>".format(self.name)
+        return "<XNATExperiment {}>".format(self.name)
 
     def __repr__(self):
         return self.__str__()
 
 
-class XNATScan(object):
+class XNATSeries(XNATObject):
 
-    def __init__(self, session_name, experiment_name, project_name, scan_json):
-        self.project = project_name
-        self.session = session_name
-        self.experiment = experiment_name
-        self.uid = str(scan_json['data_fields']['UID'])
+    def __init__(self, experiment_name, scan_json):
         self.raw_json = scan_json
+        self.experiment = experiment_name
+        self.uid = self._get_field('UID')
+        self.series = self._get_field('ID')
+        self.image_type = self._get_field('parameters/imageType')
         self.multiecho = self.is_multiecho()
-        self.series = scan_json['data_fields']['ID']
         self.description = self._set_description()
-        self.image_type = scan_json['data_fields'].get('parameters/imageType')
 
     def _set_description(self):
-        if 'series_description' in self.raw_json['data_fields'].keys():
-            return self.raw_json['data_fields']['series_description']
-        elif 'type' in self.raw_json['data_fields'].keys():
-            return self.raw_json['data_fields']['type']
-        return None
+        series_descr = self._get_field('series_description')
+        if series_descr:
+            return series_descr
+        return self._get_field('type')
 
     def is_multiecho(self):
         try:
@@ -1162,7 +1144,8 @@ class XNATScan(object):
         names = []
         self.echo_dict = {}
         for tag in tag_settings:
-            name = "_".join([self.session, tag, padded_series, mangled_descr])
+            name = "_".join([self.experiment, tag, padded_series,
+                             mangled_descr])
             if self.multiecho:
                 echo_num = tag_settings[tag]['EchoNumber']
                 if echo_num not in self.echo_dict:
@@ -1178,7 +1161,7 @@ class XNATScan(object):
         return re.sub(r"[^a-zA-Z0-9.+]+", "-", self.description)
 
     def __str__(self):
-        return "<XNATScan {} - {}>".format(self.session, self.series)
+        return "<XNATSeries {} - {}>".format(self.experiment, self.series)
 
     def __repr__(self):
         return self.__str__()
