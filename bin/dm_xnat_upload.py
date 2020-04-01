194777c44ba25fd30ab94165013b87abce2bf07c
Dawn E. Smith <Dawn.Smith@camh.ca>
2020-04-01 13:31:04 -0400

    [FIX]: zips without scans were receiving experiment instead of resource list.

--- bin/dm_xnat_upload.py
+++ bin/dm_xnat_upload.py
@@ -320,8 +320,10 @@ def check_files_exist(archive, xnat_experiment, xnat):
         logger.error("Failed getting zip file headers for: {}".format(archive))
         return False, False
 
+    xnat_resources = xnat_experiment.get_resources(xnat)
+
     if not local_headers:
-        resources_exist = resource_data_exists(xnat_experiment, archive)
+        resources_exist = resource_data_exists(xnat_resources, archive)
         return True, resources_exist
 
     if not xnat_experiment.scans:
@@ -334,7 +336,6 @@ def check_files_exist(archive, xnat_experiment, xnat):
         # Return true for both to prevent XNAT being modified
         return True, True
 
-    xnat_resources = xnat_experiment.get_resources(xnat)
     resources_exist = resource_data_exists(xnat_resources, archive)
 
     return scans_exist, resources_exist
