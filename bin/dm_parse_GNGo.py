9ec2e4b391ba813d54cd578234cfdc2eeada7e5c
Thomas Tan <Thomas.Tan@camh.ca>
2020-09-30 13:58:10 -0400

    Re-edit parser for GNGo and NBack

--- bin/dm_parse_GNGo.py
+++ bin/dm_parse_GNGo.py
@@ -3,10 +3,10 @@
 Parse the Go-NoGo eprime text files to BIDS tsv
 
 Usage:
-  parse_eprime_GNGo.py [options] <inputfile>
+  parse_eprime_GNGo.py [options] <input_file>
 
 Arguments:
-    <inputfile>             The location of the text file to parse.
+    <input_file>           The location of the text file to parse.
 
 Options:
   -o, --output FILE        Name of output tsv file
@@ -25,108 +25,114 @@ Modified to parse task file into tsv bid-format by Thomas Tan, August 22,2020
 import pandas as pd
 from docopt import docopt
 
-arguments = docopt(__doc__)
-inputfile = arguments["<inputfile>"]
-resultsfile = arguments["--output"]
-DEBUG = arguments["--debug"]
-DRYRUN = arguments["--dry-run"]
 
-if DEBUG:
-    print(arguments)
+def main():
+    arguments = docopt(__doc__)
+    input_file = arguments["<input_file>"]
+    results_file = arguments["--output"]
+    DEBUG = arguments["--debug"]
+    if DEBUG:
+        print(arguments)
+    # Loop over the inputfiles
+    if DEBUG:
+        print("\n file: {}".format(input_file))
 
-# Loop over the inputfiles
-if DEBUG:
-    print("\n file: {}".format(inputfile))
+    try:
+        with open(input_file, "r", encoding="utf-16") as my_file:
+            file_text = my_file.read()
+    except FileNotFoundError:
+        print("ERROR opening file")
+        quit()
 
-try:
-    with open(inputfile, "r", encoding="utf-16") as myfile:
-        filetext = myfile.read()
-except myfile.DoesNotExist:
-    print("ERROR opening file")
-    quit()
+    # Find and replace this weird text string that show up between every character
+    file_text = file_text.replace("\x00", "")
+    file_text = file_text.replace("\r\n", "\n")
 
-# Find and replace this weird text string that show up between every character
-filetext = filetext.replace("\x00", "")
-filetext = filetext.replace("\r\n", "\n")
+    # Split all the text into individual trials using "LogFrame End" text string
+    # This string is printed to the log at the conclusion of everytrial
+    trials = file_text.split("LogFrame End")
 
-# Split all the text into individual trials using "LogFrame End" text string
-# This string is printed to the log at the conclusion of everytrial
-trials = filetext.split("LogFrame End")
+    # Create a list to store dictionaries
+    message_df_dict = []
 
-# Create a list to store dictionaries
-message_df_dict = []
+    # Loop over individual trials
+    trial_count = 0
+    for trial in trials:
+        # For each trial collect the CorrectResponse, Subject's Response and
+        # the Reaction Time
+        Correct_Response = int()
+        Reaction_Time = int()
+        Subject_Response = int()
+        Trial_Type = str()
+        Stim_File = str()
 
-# Loop over individual trials
-trial_count = 0
-for trial in trials:
-    # For each trial collect the CorrectResponse, Subject's Response and
-    # the Reaction Time
-    CorrectResponse = int()
-    ReactionTime = int()
-    SubjectResponse = int()
-    TrialType = str()
-    StimFile = str()
+        for line in trial.split("\n\t"):
+            if "type:" in line:
+                Trial_Type = line.split(":")[1].replace(" ", "")
+            if ("ImageDisplay1.RT:" in line) and (
+                "ImageDisplay1.RTTime:" not in line
+            ):
+                Reaction_Time = line.split(":")[1].replace(" ", "")
+            if "ImageDisplay1.RESP:" in line:
+                Subject_Response = line.split(":")[1].replace(" ", "")
+            if "ImageDisplay1.CRESP:" in line:
+                Correct_Response = line.split(":")[1].replace(" ", "")
+            if "image:" in line:
+                Stim_File = line.split(":")[1].replace(" ", "")
+        # Skip if list type
+        if Trial_Type == "":
+            continue
+        trial_count += 1
 
-    for line in trial.split("\n\t"):
-        if "type:" in line:
-            TrialType = line.split(":")[1].replace(" ", "")
-        if ("ImageDisplay1.RT:" in line) and (
-            "ImageDisplay1.RTTime:" not in line
-        ):
-            ReactionTime = line.split(":")[1].replace(" ", "")
-        if "ImageDisplay1.RESP:" in line:
-            SubjectResponse = line.split(":")[1].replace(" ", "")
-        if "ImageDisplay1.CRESP:" in line:
-            CorrectResponse = line.split(":")[1].replace(" ", "")
-        if "image:" in line:
-            StimFile = line.split(":")[1].replace(" ", "")
-    # Skip if list type
-    if TrialType == "":
-        continue
-    trial_count += 1
+        # Form trial dictionary
+        trial_dict = {
+            "trial_type": Trial_Type,
+            "correct_response": Correct_Response,
+            "participant_response": Subject_Response,
+            "response_time": Reaction_Time,
+            "stim_file": Stim_File,
+            "duration": 0,
+        }
 
-    # Form trial dictionary
-    trial_dict = {
-        "trial_type": TrialType,
-        "correct_response": CorrectResponse,
-        "participant_response": SubjectResponse,
-        "response_time": ReactionTime,
-        "stim_file": StimFile,
-        "duration": 0,
-    }
+        # Stack dictionaries
+        message_df_dict.append(trial_dict)
 
-    # Stack dictionaries
-    message_df_dict.append(trial_dict)
+    # Create a dataframe comprised of the list of messages
+    # containing the information we need
+    message_df = pd.DataFrame.from_dict(message_df_dict)
 
-# Create a dataframe comprised of the list of messages
-# containing the information we need
-message_df = pd.DataFrame.from_dict(message_df_dict)
+    # Map trial type to new columns using string
+    int_to_type = {"1": "go", "2": "nogo"}
+    message_df["trial_type"] = (
+        message_df["trial_type"]
+        .str[-1]
+        .apply(lambda x: int_to_type[(x.lower())])
+    )
 
-# Map trial type to new columns using string
-int_to_type = {"1": "go", "2": "nogo"}
-message_df["trial_type"] = (
-    message_df["trial_type"].str[-1].apply(lambda x: int_to_type[(x.lower())])
-)
+    # Map response time to new columns using integers
+    # Convert response time to seconds
+    message_df["response_time"] = message_df["response_time"].astype(int)
+    message_df["response_time"] = message_df["response_time"].apply(
+        lambda x: x / 1000
+    )
 
-# Map response time to new columns using integers
-# Convert response time to seconds
-message_df["response_time"] = message_df["response_time"].astype(int)
-message_df["response_time"] = message_df["response_time"].apply(
-    lambda x: x / 1000
-)
+    # Load in the gngo onset time csv file
+    try:
+        df_onset = pd.read_csv("gng_event_timing.csv")
+    except FileNotFoundError:
+        print("ERROR opening onset time file")
+        quit()
 
-# Load in the gngo onset time csv file
-try:
-    df_onset = pd.read_csv("gng_event_timing.csv")
-except df_onset.DoesNotExist:
-    print("ERROR opening onset time file")
-    quit()
+    # Attach the onset column to the original DataFrame
+    # Reorder the columns
+    message_df = message_df.join(df_onset["onset"])
+    cols = message_df.columns.tolist()
+    cols = cols[-1:] + cols[:-1]
+    message_df = message_df[cols]
 
-# Attach the onset column to the original DataFrame
-# Reorder the columns
-message_df = message_df.join(df_onset["onset"])
-cols = message_df.columns.tolist()
-cols = cols[-1:] + cols[:-1]
-message_df = message_df[cols]
-# Output the tsv file
-message_df.to_csv(resultsfile, sep="\t", index=False)
+    # Output the tsv file
+    message_df.to_csv(results_file, sep="\t", index=False)
+
+
+if __name__ == "__main__":
+    main()
--- bin/dm_parse_Nback.py
+++ bin/dm_parse_Nback.py
@@ -3,10 +3,10 @@
 Parse the N-back eprime text files into BIDS tsvs
 
 Usage:
-  parse_eprime_Nback.py [options] <inputfile>
+  parse_eprime_Nback.py [options] <input_file>
 
 Arguments:
-    <inputfile>            The location of the ePRIME text file to parse.
+    <input_file>            The location of the ePRIME text file to parse.
 
 Options:
   -o, --output FILE        Name of output tsv file
@@ -25,118 +25,125 @@ Modified to parse task file to bids tsv format by Thomas Tan, Aug 2020
 import pandas as pd
 from docopt import docopt
 
-arguments = docopt(__doc__)
-inputfile = arguments["<inputfile>"]
-resultsfile = arguments["--output"]
-DEBUG = arguments["--debug"]
-DRYRUN = arguments["--dry-run"]
 
-if DEBUG:
-    print(arguments)
+def main():
+    arguments = docopt(__doc__)
+    input_file = arguments["<input_file>"]
+    results_file = arguments["--output"]
+    DEBUG = arguments["--debug"]
 
-corr = {"c": 1, "0": 0, "1": 1, "3": 1}
+    if DEBUG:
+        print(arguments)
 
-# loop over the inputfiles
-if DEBUG:
-    print("\n file: {}".format(inputfile))
+    corr = {"c": 1, "0": 0, "1": 1, "3": 1}
 
-with open(inputfile, "r", encoding="utf-16") as myfile:
-    filetext = myfile.read()
+    # loop over the inputfiles
+    if DEBUG:
+        print("\n file: {}".format(input_file))
 
-# find and replace this weird text string that show up between every character
-filetext = filetext.replace("\x00", "")
-filetext = filetext.replace("\r\n", "\n")  # a windows formatting thing..
+    with open(input_file, "r", encoding="utf-16") as my_file:
+        file_text = my_file.read()
 
-# split all the text into individual trials using "LogFrame End" text string
-# this string is printed to the log at the conclusion of everytrial
-trials = filetext.split("LogFrame End")
+    # find and replace this weird text string that show up between every character
+    file_text = file_text.replace("\x00", "")
+    file_text = file_text.replace("\r\n", "\n")  # a windows formatting thing..
 
-# Create a list to store dictionaries
-message_df_dict = []
+    # split all the text into individual trials using "LogFrame End" text string
+    # this string is printed to the log at the conclusion of everytrial
+    trials = file_text.split("LogFrame End")
 
-# loop over individual trials
-trial_count = 0
-for trial in trials:
-    # for each trial collect CorrectResponse, Subject's Response
-    #  and the Reaction Time
-    CorrectResponse = int()
-    ReactionTime = int()
-    SubjectResponse = int()
-    TrialType = str()
-    TrialTime = int()
-    for line in trial.split("\n\t"):
-        if "Running:" in line:
-            TrialType = line.split(":")[1].replace(" ", "")
-        if "letterdisp.OnsetTime:" in line:
-            TrialTime = line.split(":")[1].replace(" ", "")
-        if (
-            ("resp:" in line)
-            and ("letterdisp.RESP" not in line)
-            and ("letterdisp.CRESP" not in line)
-        ):
-            CorrectResponse = line.split(":")[1].replace(" ", "")
-        if ("letterdisp.RT" in line) and ("letterdisp.RTTime" not in line):
-            ReactionTime = line.split(":")[1].replace(" ", "")
-        if "letterdisp.RESP" in line:
-            SubjectResponse = line.split(":")[1].replace(" ", "")
+    # Create a list to store dictionaries
+    message_df_dict = []
 
-    # Skip if list type
-    if (TrialType == "List1") or (TrialType == ""):
-        continue
-    trial_count += 1
+    # loop over individual trials
+    trial_count = 0
+    for trial in trials:
+        # for each trial collect CorrectResponse, Subject's Response
+        #  and the Reaction Time
+        Correct_Response = int()
+        Reaction_Time = int()
+        Subject_Response = int()
+        Trial_Type = str()
+        Trial_Time = int()
+        for line in trial.split("\n\t"):
+            if "Running:" in line:
+                Trial_Type = line.split(":")[1].replace(" ", "")
+            if "letterdisp.OnsetTime:" in line:
+                Trial_Time = line.split(":")[1].replace(" ", "")
+            if (
+                ("resp:" in line)
+                and ("letterdisp.RESP" not in line)
+                and ("letterdisp.CRESP" not in line)
+            ):
+                Correct_Response = line.split(":")[1].replace(" ", "")
+            if ("letterdisp.RT" in line) and ("letterdisp.RTTime" not in line):
+                Reaction_Time = line.split(":")[1].replace(" ", "")
+            if "letterdisp.RESP" in line:
+                Subject_Response = line.split(":")[1].replace(" ", "")
 
-    # Form trial dictionary
-    trial_dict = {
-        "onset": TrialTime,
-        "duration": 0,
-        "trial_type": TrialType,
-        "correct_response": corr[str(CorrectResponse)],
-        "participant_response": 1 if SubjectResponse else 0,
-        "response_time": ReactionTime,
-    }
-    # Stack dictionaries
-    message_df_dict.append(trial_dict)
+        # Skip if list type
+        if (Trial_Type == "List1") or (Trial_Type == ""):
+            continue
+        trial_count += 1
 
-# Create a dataframe comprised of the list of messages containing
-#  the information we need
-message_df = pd.DataFrame.from_dict(message_df_dict)
+        # Form trial dictionary
+        trial_dict = {
+            "onset": Trial_Time,
+            "duration": 0,
+            "trial_type": Trial_Type,
+            "correct_response": corr[str(Correct_Response)],
+            "participant_response": 1 if Subject_Response else 0,
+            "response_time": Reaction_Time,
+        }
+        # Stack dictionaries
+        message_df_dict.append(trial_dict)
 
-# Map blocks and trial type to new columns
-message_df["block"] = message_df["trial_type"].str[-1]
-message_df["trial_type"] = message_df["trial_type"].str[:-1]
+    # Create a dataframe comprised of the list of messages containing
+    #  the information we need
+    message_df = pd.DataFrame.from_dict(message_df_dict)
 
-# Map response time to new columns in seconds
-message_df["response_time"] = message_df["response_time"].astype(int)
-message_df["response_time"] = message_df["response_time"].apply(
-    lambda x: x / 1000
-)
+    # Map blocks and trial type to new columns
+    message_df["block"] = message_df["trial_type"].str[-1]
+    message_df["trial_type"] = message_df["trial_type"].str[:-1]
 
-# Convert the onset time to seconds
-# Account for 4 TR drops and first trial start time is 15 - 8
-message_df["onset"] = message_df["onset"].astype(int)
-message_df["onset"] = (
-    message_df["onset"].sub(message_df.iloc[0, :]["onset"]) / 1000 + 15 - 8
-)
-
-# Create a dataframe comprised of onset of blocks and duration
-df1 = (
-    message_df.iloc[0::20]
-    .assign(
-        duration=lambda x: x["duration"] + 60,
-        trial_type=lambda x: x["trial_type"] + "block",
+    # Map response time to new columns in seconds
+    message_df["response_time"] = message_df["response_time"].astype(int)
+    message_df["response_time"] = message_df["response_time"].apply(
+        lambda x: x / 1000
     )
-    .rename(lambda x: x - 0.5)
-)
-# Concatenate the new dataframe into the original dataframe
-message_df_final = (
-    pd.concat([message_df, df1], sort=False).sort_index().reset_index(drop=True)
-)
-# Map the correct response, participant response,
-# and reponse time columns back to n/a
-cols = ["correct_response", "participant_response", "response_time"]
-message_df_final.loc[0::21, (cols)] = message_df_final.loc[
-    0::21, (cols)
-].replace(0, "n/a")
 
-# Output the dataframe output as tsv file
-message_df_final.to_csv(resultsfile, sep="\t", index=False)
+    # Convert the onset time to seconds
+    # Account for 4 TR drops and first trial start time is 15 - 8
+    message_df["onset"] = message_df["onset"].astype(int)
+    message_df["onset"] = (
+        message_df["onset"].sub(message_df.iloc[0, :]["onset"]) / 1000 + 15 - 8
+    )
+
+    # Create a dataframe comprised of onset of blocks and duration
+    df1 = (
+        message_df.iloc[0::20]
+        .assign(
+            duration=lambda x: x["duration"] + 60,
+            trial_type=lambda x: x["trial_type"] + "block",
+        )
+        .rename(lambda x: x - 0.5)
+    )
+    # Concatenate the new dataframe into the original dataframe
+    message_df_final = (
+        pd.concat([message_df, df1], sort=False)
+        .sort_index()
+        .reset_index(drop=True)
+    )
+    # Map the correct response, participant response,
+    # and reponse time columns back to n/a
+    cols = ["correct_response", "participant_response", "response_time"]
+    message_df_final.loc[0::21, (cols)] = message_df_final.loc[
+        0::21, (cols)
+    ].replace(0, "n/a")
+
+    # Output the dataframe output as tsv file
+    message_df_final.to_csv(results_file, sep="\t", index=False)
+
+
+if __name__ == "__main__":
+    main()
