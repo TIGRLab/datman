e29e268e0c164f7def441c8f1e937ab80974bb48
Dawn Smith <dawn.smith@camh.ca>
2022-03-18 13:51:50 -0400

    [ENH] Add tests for add_scan_length

--- tests/test_dm_qc_report.py
+++ tests/test_dm_qc_report.py
@@ -3,7 +3,7 @@ import logging
 import importlib
 
 import pytest
-from mock import patch
+from mock import patch, Mock
 
 import datman.config
 import datman.exceptions
@@ -21,6 +21,40 @@ config = datman.config.config(
     study="STUDY"
 )
 
+
+@patch.dict(os.environ, {"DM_CONFIG": FIXTURE_SETTINGS,
+                         "DM_SYSTEM": FIXTURE_SYSTEM}
+)
+class TestGetConfig:
+
+    @patch("os.path.exists")
+    def test_exits_gracefully_with_bad_study(self, mock_exists):
+        mock_exists.return_value = True
+        with pytest.raises(datman.exceptions.ConfigException):
+            qc.get_config(study="madeupcode")
+
+    @patch('datman.config.config')
+    def test_exits_gracefully_when_paths_missing_from_config(
+            self, mock_config):
+        def mock_get_path(key):
+            if key == "nii":
+                return ""
+            raise datman.exceptions.UndefinedSetting
+        mock_config.return_value.get_path = mock_get_path
+
+        with pytest.raises(datman.exceptions.UndefinedSetting):
+            qc.get_config("STUDY")
+
+    @patch("os.path.exists")
+    def test_returns_config_when_paths_exist_and_study_defined(
+            self, mock_exists):
+        mock_exists.return_value = True
+        config = qc.get_config(study="STUDY")
+        assert config is not None
+        assert type(config) == datman.config.config
+
+
+# class TestSubmitSubjects:
 
 @patch("bin.dm_qc_report.docopt")
 class TestMakeQCCommand:
@@ -70,37 +104,6 @@ class TestMakeQCCommand:
         assert result == f"{qc.__file__} {self.study} {self.subid}"
 
 
-@patch.dict(os.environ, {"DM_CONFIG": FIXTURE_SETTINGS,
-                         "DM_SYSTEM": FIXTURE_SYSTEM}
-)
-class TestGetConfig:
-
-    @patch("os.path.exists")
-    def test_returns_config_when_paths_exist_and_study_defined(
-            self, mock_exists):
-        mock_exists.return_value = True
-        config = qc.get_config(study="STUDY")
-        assert config is not None
-        assert type(config) == datman.config.config
-
-    @patch("os.path.exists")
-    def test_exits_gracefully_with_bad_study(self, mock_exists):
-        mock_exists.return_value = True
-        with pytest.raises(datman.exceptions.ConfigException):
-            qc.get_config(study="madeupcode")
-
-    @patch('datman.config.config')
-    def test_exits_gracefully_when_paths_missing_from_config(
-            self, mock_config):
-        def mock_get_path(key):
-            if key == "nii":
-                return ""
-            raise datman.exceptions.UndefinedSetting
-        mock_config.return_value.get_path = mock_get_path
-
-        with pytest.raises(datman.exceptions.UndefinedSetting):
-            qc.get_config("STUDY")
-
 
 class TestPrepareScan:
     subid = "STUDY_SITE_ID_01"
@@ -127,5 +130,41 @@ class TestPrepareScan:
         assert mock_define.call_count == 1
 
 
-# Test is_outdated_header_diffs from dashboard side
-# Test header qc runs when header qc is outdated from datman side
+class TestAddScanLength:
+
+    nii_path = "./STUDY_CMH_0001_01_01.nii.gz"
+
+    def test_no_exception_if_file_cant_be_read(self):
+        mock_db_scan = Mock()
+        assert not os.path.exists(self.nii_path)
+        qc.add_scan_length(self.nii_path, mock_db_scan)
+
+    @patch("bin.dm_qc_report.nib")
+    def test_database_updated_with_length(self, mock_nib):
+        shape = [1, 1, 1, 999]
+        def mock_load(path):
+            if path == self.nii_path:
+                data = Mock()
+                data.shape = shape
+                return data
+            raise Exception
+        mock_nib.load = mock_load
+        mock_db_scan = Mock()
+        qc.add_scan_length(self.nii_path, mock_db_scan)
+
+        assert mock_db_scan.length == str(shape[3])
+
+    @patch("bin.dm_qc_report.nib")
+    def test_scan_without_fourth_dimension_gets_length_na(self, mock_nib):
+        shape = [1, 1, 1]
+        def mock_load(path):
+            if path == self.nii_path:
+                data = Mock()
+                data.shape = shape
+                return data
+            raise Exception
+        mock_nib.load = mock_load
+        mock_db_scan = Mock()
+        qc.add_scan_length(self.nii_path, mock_db_scan)
+
+        assert mock_db_scan.length == "N/A"
